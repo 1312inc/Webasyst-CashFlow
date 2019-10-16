47fdf35343f231a12db1761eb1d0193edd76419f
vofka <vofka@webasyst.com>
2019-10-16 13:28:50 +0300

    Initial commit

---
+++ lib/config/app.php
@@ -0,0 +1,12 @@
+<?php
+return array (
+  'name' => 'Cash',
+  'icon' =>
+  array (
+    48 => 'img/cash48.png',
+    96 => 'img/cash96.png',
+  ),
+  'version' => '1.0.0',
+  'vendor' => '1021997',
+  'plugins' => false,
+);
