05031250dbd44be702d76d6cfcc6c7ce0e5021f0
Kirill Maramygin <kirill.maramygin@gmail.com>
2020-01-22 07:28:15 +0300

    7.94:sort currencies by code + select the currency based on wa->locale()

--- lib/actions/account/cashAccountDialog.action.php
+++ lib/actions/account/cashAccountDialog.action.php
@@ -27,6 +27,27 @@ class cashAccountDialogAction extends cashViewAction
             $currencies[$currency['code']] = cashCurrencyVO::fromWaCurrency($currency['code']);
         }
 
+        uasort($currencies, static function (cashCurrencyVO $a, cashCurrencyVO $b) {
+            return $a->getCode() > $b->getCode();
+        });
+
+        switch (wa()->getUser()->getLocale()) {
+            case 'ru_RU':
+                $selectedCurrency = 'RUB';
+                break;
+
+            case 'uk_UA':
+            case 'ua_UA':
+                $selectedCurrency = 'UAH';
+                break;
+
+            default:
+                $selectedCurrency = 'USD';
+                break;
+        }
+
+        $account->setCurrency($selectedCurrency);
+
         $this->view->assign(
             [
                 'account' => cashDtoFromEntityFactory::fromEntity(cashAccountDto::class, $account),
