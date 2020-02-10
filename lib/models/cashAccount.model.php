a628e0503e57caae97c7f9f086c290ec8fd56216
Kirill Maramygin <kirill.maramygin@gmail.com>
2020-02-10 23:09:22 +0300

    7.111:show totals by category under the table

--- lib/models/cashAccount.model.php
+++ lib/models/cashAccount.model.php
@@ -61,6 +61,43 @@ SQL;
     }
 
     /**
+     * @param string     $startDate
+     * @param string     $endDate
+     * @param string     $filterType
+     * @param null|array $filterIds
+     *
+     * @return array
+     */
+    public function getStatDetailedCategoryData($startDate, $endDate, $filterType = 'account', $filterIds = null)
+    {
+        $filterSql = '';
+        if ($filterIds) {
+            $filterSql = ($filterType === 'account') ? ' and ca.id in (i:filter_ids)' : ' and cc.id in (i:filter_ids)';
+        }
+
+        $sql = <<<SQL
+select cc.name,
+       cc.color,
+       ct.category_id id,
+       ca.currency,
+       sum(if(ct.amount > 0, ct.amount, 0)) income,
+       sum(if(ct.amount < 0, ct.amount, 0)) expense,
+       sum(ct.amount)                       summary
+from cash_transaction ct
+         join cash_account ca on ct.account_id = ca.id
+         left join cash_category cc on ct.category_id = cc.id
+where ct.date between s:startDate and s:endDate
+      and ca.is_archived = 0
+      {$filterSql}
+group by ct.category_id, ca.currency
+SQL;
+
+        return $this
+            ->query($sql, ['startDate' => $startDate, 'endDate' => $endDate, 'filter_ids' => (array)$filterIds])
+            ->fetchAll();
+    }
+
+    /**
      * @param string $startDate
      * @param string $endDate
      * @param array  $categories
