3d94609528cbf261ee696b7e479833d72a42b1a7
Kirill Maramygin <kirill.maramygin@gmail.com>
2020-02-18 23:46:04 +0300

    7.135:initial account & category list setup during the installation

--- lib/config/install.php
+++ lib/config/install.php
@@ -55,3 +55,32 @@ if ($innodb) {
 } else {
     throw new waException('InnoDb engine is required');
 }
+
+cash()->getEntityPersister()->insert(
+    (new cashAccount())
+        ->setName(wa()->accountName())
+        ->setCurrency(wa()->getLocale() === 'en_US' ? 'USD' : 'RUB')
+        ->setIcon('star')
+);
+
+$fixtures = [
+    cashCategory::TYPE_INCOME => [
+        _w('Sales') => '#00dd00',
+        _w('Investment') => '#001100',
+    ],
+    cashCategory::TYPE_EXPENSE => [
+        _w('Salary') => '#dd0000',
+        _w('Tax') => '#110000',
+    ],
+];
+
+foreach ($fixtures as $type => $categories) {
+    foreach ($categories as $name => $color) {
+        cash()->getEntityPersister()->insert(
+            (new cashCategory())
+                ->setType($type)
+                ->setColor($color)
+                ->setName($name)
+        );
+    }
+}
