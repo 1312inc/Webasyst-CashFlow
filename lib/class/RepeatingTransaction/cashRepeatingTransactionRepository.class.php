6b52d60c04595929d31e2b46e044b8ec7062d111
Kirill Maramygin <kirill.maramygin@gmail.com>
2020-02-20 09:30:39 +0300

    7.112:repeating transactions

--- lib/class/RepeatingTransaction/cashRepeatingTransactionRepository.class.php
+++ lib/class/RepeatingTransaction/cashRepeatingTransactionRepository.class.php
@@ -23,7 +23,7 @@ from cash_repeating_transaction crt
 left join (
     select count(repeating_id) repeatings, repeating_id, max(date) last_transaction_date from cash_transaction where date > s:date group by repeating_id
 ) ct on ct.repeating_id = crt.id
-where crt.repeating_end_type = 'never'
+where crt.repeating_end_type = 'never' and crt.enabled = 1
 SQL;
 
         $transToRepeat = [];
