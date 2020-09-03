0a8eb2e0039cf41e6688f34bfbeec96d14e40c56
Konstantin Zaytsev <zaytsev.const@gmail.com>
2020-09-03 23:58:57 +0300

    App implementation

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <h2>It works!</h2>
+    <p>Router works too!</p>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -7,3 +9,13 @@
     <router-view/>
   </div>
 </template>
+
+<style lang="scss" scoped>
+  #app {
+    text-align: center;
+    padding-top: 4rem;
+  }
+  h2 {
+    margin-bottom: 4rem;
+  }
+</style>
