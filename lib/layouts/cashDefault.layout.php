3f6d67596126a3f4221fa5ff29d200052f1a9961
Kirill Maramygin <kirill.maramygin@gmail.com>
2020-09-07 07:31:46 +0300

    7.349:webasyst2: add 1-day API token into Default.html

--- lib/layouts/cashDefault.layout.php
+++ lib/layouts/cashDefault.layout.php
@@ -10,16 +10,19 @@ class cashDefaultLayout extends waLayout
      */
     public function execute()
     {
+        if (!cash()->getUser()->hasAccessToApp()) {
+            throw new kmwaForbiddenException('No app access');
+        }
+
+        $token = (new cashApiToken())->retrieveToken(cash()->getUser()->getContact());
         $this->executeAction('sidebar', new cashBackendSidebarAction());
-        $showReviewWidget = cash()->getModel(cashTransaction::class)
-                ->select('count(id)')
-                ->limit(500)
-                ->fetchField() == 500;
+        $showReviewWidget = cash()->getModel(cashTransaction::class)->select('count(id)')->limit(30)->fetchField() == 30;
 
         $this->view->assign(
             [
+                'token' => $token,
                 'content' => '<i class="icon16 loading"></i>',
-                'isAdmin' => (int) cash()->getUser()->isAdmin(),
+                'isAdmin' => (int) cash()->getUser()->canImport(),
                 'contextUser' => cash()->getUser(),
                 'userId' => (int) wa()->getUser()->getId(),
                 'show_review_widget' => $showReviewWidget,
