ed5b15f929f7fc2cacfd4f42cd8fd2189c6371ae
Konstantin Zaytsev <zaytsev.const@gmail.com>
2020-09-07 22:59:32 +0300

    API connected and amcharts replaced by Chart.js

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   publicPath: '/webasyst/cash/',
   productionSourceMap: false,
-  filenameHashing: false
+  filenameHashing: false,
+  devServer: {
+    proxy: 'http://localhost:8888'
+  }
 }
