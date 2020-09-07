b46455610a2ee6aef9879226c2d160104a125752
Konstantin Zaytsev <zaytsev.const@gmail.com>
2020-09-07 22:59:32 +0300

    API connected and amcharts replaced by Chart.js

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -2,92 +2,56 @@
   <div class="about">
     <h1>This is an about page</h1>
 
-    <div id="chartdiv"></div>
+    <canvas ref="chart"></canvas>
   </div>
 </template>
 
 <script>
-import * as am4core from '@amcharts/amcharts4/core'
-import * as am4charts from '@amcharts/amcharts4/charts'
-// eslint-disable-next-line camelcase
-// import am4themes_animated from '@amcharts/amcharts4/themes/animated'
+import Chart from 'chart.js'
 
 export default {
   mounted () {
-    /* Chart code */
-    // Themes begin
-    // am4core.useTheme(am4themes_animated)
-    // Themes end
-
-    // Create chart instance
-    const chart = am4core.create('chartdiv', am4charts.XYChart)
-
-    // Add data
-    chart.data = [{
-      year: '2003',
-      europe: 2.5,
-      namerica: 2.5,
-      asia: 2.1,
-      lamerica: 1.2,
-      meast: 0.2,
-      africa: 0.1
-    }, {
-      year: '2004',
-      europe: 2.6,
-      namerica: 2.7,
-      asia: 2.2,
-      lamerica: 1.3,
-      meast: 0.3,
-      africa: 0.1
-    }, {
-      year: '2005',
-      europe: 2.8,
-      namerica: 2.9,
-      asia: 2.4,
-      lamerica: 1.4,
-      meast: 0.3,
-      africa: 0.1
-    }]
-
-    // Create axes
-    const categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis())
-    categoryAxis.dataFields.category = 'year'
-    categoryAxis.title.text = 'Local country offices'
-    categoryAxis.renderer.grid.template.location = 0
-    categoryAxis.renderer.minGridDistance = 20
-    categoryAxis.renderer.cellStartLocation = 0.1
-    categoryAxis.renderer.cellEndLocation = 0.9
-
-    const valueAxis = chart.yAxes.push(new am4charts.ValueAxis())
-    valueAxis.min = 0
-    valueAxis.title.text = 'Expenditure (M)'
-
-    // Create series
-    function createSeries (field, name, stacked) {
-      const series = chart.series.push(new am4charts.ColumnSeries())
-      series.dataFields.valueY = field
-      series.dataFields.categoryX = 'year'
-      series.name = name
-      series.columns.template.tooltipText = '{name}: [bold]{valueY}[/]'
-      series.stacked = stacked
-      series.columns.template.width = am4core.percent(95)
-    }
-
-    createSeries('europe', 'Europe', false)
-    createSeries('namerica', 'North America', true)
-    createSeries('asia', 'Asia', false)
-    createSeries('lamerica', 'Latin America', true)
-    createSeries('meast', 'Middle East', true)
-    createSeries('africa', 'Africa', true)
-
-    // Add legend
-    chart.legend = new am4charts.Legend()
-    this.chart = chart
+    this.chart = new Chart(this.$refs.chart, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    })
   },
 
   beforeDestroy () {
     if (this.chart) {
-      this.chart.dispose()
+      this.chart.destroy()
     }
   }
 }
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="home">
     <h1>This is an home page</h1>
+
+    {{items}}
   </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: mapState(['items']),
+  mounted () {
+    this.$store.dispatch('get')
+  }
+}
+</script>
