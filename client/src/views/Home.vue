bfd9a99eb294b35977c5c92e2242426b0706fcbb
Konstantin Zaytsev <zaytsev.const@gmail.com>
2020-09-10 00:24:04 +0300

    Chart optimization and some plugins

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,66 +1,46 @@
 <template>
   <div class="home">
-    <canvas ref="chart"></canvas>
+
+    <Chart></Chart>
+
+    <table>
+      <thead>
+        <tr>
+          <th>Дата</th>
+          <th>Сумма</th>
+          <th>Категория</th>
+          <th>Описание</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="transaction in listItems"
+          :key="transaction.id"
+        >
+          <td>{{$moment(transaction.date).format('ll')}}</td>
+          <td>{{$numeral(+transaction.amount).format('0,0 $')}}</td>
+          <td>{{transaction.category_id}}</td>
+          <td>{{transaction.description}}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-import Chart from 'chart.js'
-import moment from 'moment'
+import { mapState, mapActions } from 'vuex'
+import Chart from '@/components/Chart'
 
 export default {
-  computed: {
-    datasets () {
-      const colors = ['rgba(255, 99, 22, 1)', 'rgba(22, 99, 132, 1)', 'rgba(255, 199, 132, 1)', 'rgba(255, 99, 232, 1)', 'rgba(253, 99, 132, 1)', 'rgba(55, 99, 132, 1)', '#3ad12e', '#000', '#000']
-      let colorsCount = 0
-      const res = this.$store.state.items.reduce((acc, e) => {
-        if (!acc[`category_${e.category_id}`]) {
-          acc[`category_${e.category_id}`] = {
-            label: e.category_id,
-            stack: +e.amount > 0 ? 'Stack Plus' : 'Stack Minus',
-            data: [],
-            backgroundColor: colors[colorsCount]
-          }
-          colorsCount++
-        }
-        acc[`category_${e.category_id}`].data.push({ x: e.date, y: +e.amount })
-        return acc
-      }, {})
-      return Object.values(res)
-    }
+  components: {
+    Chart
   },
+  computed: mapState(['listItems']),
   mounted () {
-    moment.locale('ru')
-    this.$store.dispatch('get', { from: '2020-01-01', to: '2020-09-08' }).then(() => {
-      this.chart = new Chart(this.$refs.chart, {
-        type: 'bar',
-        data: {
-          datasets: this.datasets
-        },
-        options: {
-          tooltips: {
-            mode: 'index',
-            intersect: false
-          },
-          responsive: true,
-          scales: {
-            xAxes: [{
-              stacked: true,
-              type: 'time',
-              time: {
-                unit: 'month'
-              }
-            }],
-            yAxes: [{
-              stacked: true
-            }]
-          },
-          layout: {
-            padding: 60
-          }
-        }
-      })
-    })
+    this.getList({ from: '2019-01-01', to: '2020-09-08' })
+  },
+  methods: {
+    ...mapActions(['getList'])
   }
 }
 </script>
