96aa0710f07105df9c72cf8caa8416fa224cbe55
Konstantin Zaytsev <zaytsev.const@gmail.com>
2020-09-09 10:32:33 +0300

    Chart + API testing

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <div id="app">
-    <h2>It works!</h2>
-    <p>Router works too!</p>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
