6991a2457431e4ab3c26efd8fba491f5d0586932
Konstantin Zaytsev <zaytsev.const@gmail.com>
2020-09-09 10:32:33 +0300

    Chart + API testing

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,66 @@
 <template>
   <div class="home">
-    <h1>This is an home page</h1>
-
-    {{items}}
+    <canvas ref="chart"></canvas>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import Chart from 'chart.js'
+import moment from 'moment'
 
 export default {
-  computed: mapState(['items']),
+  computed: {
+    datasets () {
+      const colors = ['rgba(255, 99, 22, 1)', 'rgba(22, 99, 132, 1)', 'rgba(255, 199, 132, 1)', 'rgba(255, 99, 232, 1)', 'rgba(253, 99, 132, 1)', 'rgba(55, 99, 132, 1)', '#3ad12e', '#000', '#000']
+      let colorsCount = 0
+      const res = this.$store.state.items.reduce((acc, e) => {
+        if (!acc[`category_${e.category_id}`]) {
+          acc[`category_${e.category_id}`] = {
+            label: e.category_id,
+            stack: +e.amount > 0 ? 'Stack Plus' : 'Stack Minus',
+            data: [],
+            backgroundColor: colors[colorsCount]
+          }
+          colorsCount++
+        }
+        acc[`category_${e.category_id}`].data.push({ x: e.date, y: +e.amount })
+        return acc
+      }, {})
+      return Object.values(res)
+    }
+  },
   mounted () {
-    this.$store.dispatch('get')
+    moment.locale('ru')
+    this.$store.dispatch('get', { from: '2020-01-01', to: '2020-09-08' }).then(() => {
+      this.chart = new Chart(this.$refs.chart, {
+        type: 'bar',
+        data: {
+          datasets: this.datasets
+        },
+        options: {
+          tooltips: {
+            mode: 'index',
+            intersect: false
+          },
+          responsive: true,
+          scales: {
+            xAxes: [{
+              stacked: true,
+              type: 'time',
+              time: {
+                unit: 'month'
+              }
+            }],
+            yAxes: [{
+              stacked: true
+            }]
+          },
+          layout: {
+            padding: 60
+          }
+        }
+      })
+    })
   }
 }
 </script>
