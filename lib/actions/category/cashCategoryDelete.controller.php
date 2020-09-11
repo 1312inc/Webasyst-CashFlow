5d01adf705f8e128e37216ba81478f3dc64133d3
Kirill Maramygin <kirill.maramygin@gmail.com>
2020-09-11 19:33:57 +0300

    7.365:webasyst2: REST APIs cash.category.*

--- lib/actions/category/cashCategoryDelete.controller.php
+++ lib/actions/category/cashCategoryDelete.controller.php
@@ -19,6 +19,10 @@ class cashCategoryDeleteController extends cashJsonController
             throw new kmwaForbiddenException(_w('You are not allowed to access this account'));
         }
 
+        if ($category->isSystem()) {
+            throw new kmwaRuntimeException(_w('You can`t do anything with system categories'));
+        }
+
         $remover = new cashCategoryRemover(cash()->getEntityFactory(cashCategory::class));
         if (!$remover->removeCategory($category)) {
             $this->setError($remover->getError());
