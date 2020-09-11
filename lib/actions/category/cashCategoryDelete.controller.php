9cd4eb0f587ffa706c5d0d409f56eb736712d5a6
Kirill Maramygin <kirill.maramygin@gmail.com>
2020-09-11 19:33:57 +0300

    7.365:webasyst2: REST APIs cash.category.*

--- lib/actions/category/cashCategoryDelete.controller.php
+++ lib/actions/category/cashCategoryDelete.controller.php
@@ -16,14 +16,14 @@ class cashCategoryDeleteController extends cashJsonController
         kmwaAssert::instance($category, cashCategory::class);
 
         if (!cash()->getContactRights()->hasFullAccessToCategory(wa()->getUser(), $category)) {
-            throw new kmwaForbiddenException(_w('You are not allowed to access this category'));
+            throw new kmwaForbiddenException(_w('You are not allowed to access this account'));
         }
 
         if ($category->isSystem()) {
             throw new kmwaRuntimeException(_w('You can`t do anything with system categories'));
         }
 
-        $remover = new cashCategoryRemover(cash()->getEntityRepository(cashCategory::class));
+        $remover = new cashCategoryRemover(cash()->getEntityFactory(cashCategory::class));
         if (!$remover->removeCategory($category)) {
             $this->setError($remover->getError());
         }
--- lib/class/Api/cashApiAbstractMethod.class.php
+++ lib/class/Api/cashApiAbstractMethod.class.php
@@ -5,9 +5,10 @@
  */
 abstract class cashApiAbstractMethod extends waAPIMethod
 {
-    const METHOD_GET  = 'GET';
-    const METHOD_POST = 'POST';
-    const METHOD_PUT = 'PUT';
+    const METHOD_GET    = 'GET';
+    const METHOD_POST   = 'POST';
+    const METHOD_DELETE = 'DELETE';
+    const METHOD_PUT    = 'PUT';
 
     /**
      * @var array
