6b2e2f05193843dfc5151f892a2dace1d6f93a4d
Konstantin Zaytsev <zaytsev.const@gmail.com>
2020-09-14 22:43:12 +0300

    Chart: Balance negative line and dynamic chart data fetching

--- client/src/plugins/dumpDataMonth.js
+++ client/src/plugins/dumpDataMonth.js
@@ -3,7 +3,7 @@ import moment from 'moment'
 
 function randomInteger (min, max, count) {
   // получить случайное число от (min-0.5) до (max+0.5)
-  if (count && count > 50) {
+  if (count && count > 700) {
     min = -min
     max = -max
   }
@@ -14,9 +14,9 @@ function randomInteger (min, max, count) {
 const result = []
 
 let i = 0
-while (i < 100) {
+while (i < 200) {
   result.push({
-    date: moment().add(-i, 'M').format('YYYY-MM-DD'),
+    date: moment().add(-i, 'd').format('YYYY-MM-DD'),
     income: randomInteger(100000, 300000),
     expense: randomInteger(10000, 100000),
     balance: randomInteger(10000, 100000, i)
