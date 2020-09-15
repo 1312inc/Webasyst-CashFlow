a74c2eea2b2e0d66ad02ad9043d6bcd1819116e7
Konstantin Zaytsev <zaytsev.const@gmail.com>
2020-09-15 13:22:16 +0300

    Category and Account Store modules. Some styles

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,20 +1,34 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <a href="/webasyst/cash/?module=static">Static page</a>
+    </div> -->
+    <div class="flex">
+      <div class="w-1/6 bg-gray-100">
+        <Sidebar />
+      </div>
+      <div class="w-5/6 mx-10">
+        <keep-alive>
+          <router-view/>
+        </keep-alive>
+      </div>
     </div>
-    <keep-alive>
-      <router-view/>
-    </keep-alive>
+
   </div>
 </template>
 
 <script>
+import Sidebar from '@/components/Sidebar'
 export default {
+  components: {
+    Sidebar
+  },
   mounted () {
     this.$store.dispatch('transaction/getList', { from: '2019-01-01', to: '2020-09-08' })
+    this.$store.dispatch('category/getList')
+    this.$store.dispatch('account/getList')
   }
 }
 </script>
