7ddeee66ae0c7c358affee139bd103d77e52964b
Konstantin Zaytsev <zaytsev.const@gmail.com>
2020-09-15 13:22:16 +0300

    Category and Account Store modules. Some styles

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -3,24 +3,25 @@
 
     <AmChart></AmChart>
 
-    <table>
+    <table class="table-auto w-full">
       <thead>
         <tr>
-          <th>Дата</th>
-          <th>Сумма</th>
-          <th>Категория</th>
-          <th>Описание</th>
+          <th class="px-4 py-2">Дата</th>
+          <th class="px-4 py-2">Сумма</th>
+          <th class="px-4 py-2">Категория</th>
+          <th class="px-4 py-2">Описание</th>
         </tr>
       </thead>
       <tbody>
         <tr
-          v-for="transaction in listItems"
+          v-for="(transaction, i) in listItems"
           :key="transaction.id"
+          :class="{'bg-gray-100': i % 2 === 0}"
         >
-          <td>{{$moment(transaction.date).format('ll')}}</td>
-          <td>{{$numeral(+transaction.amount).format('0,0 $')}}</td>
-          <td>{{transaction.category_id}}</td>
-          <td>{{transaction.description}}</td>
+          <td class="border px-4 py-2">{{$moment(transaction.date).format('ll')}}</td>
+          <td class="border px-4 py-2">{{$numeral(+transaction.amount).format('0,0 $')}}</td>
+          <td class="border px-4 py-2">{{getCategoryNameById(transaction.category_id)}}</td>
+          <td class="border px-4 py-2">{{transaction.description}}</td>
         </tr>
       </tbody>
     </table>
@@ -28,13 +29,16 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState, mapGetters } from 'vuex'
 import AmChart from '@/components/AmChartFake'
 
 export default {
   components: {
     AmChart
   },
-  computed: mapState('transaction', ['listItems'])
+  computed: {
+    ...mapState('transaction', ['listItems']),
+    ...mapGetters('category', ['getCategoryNameById'])
+  }
 }
 </script>
