33a25413037b491bb950ebc803da729c49c71a5b
Konstantin Zaytsev <zaytsev.const@gmail.com>
2020-09-22 17:12:58 +0300

    7.397:webasyst2: subfolder fix

--- client/src/plugins/api.js
+++ client/src/plugins/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: process.env.VUE_APP_BASE_URL,
+  baseURL: process.env.VUE_APP_BASE_URL || (window.appState ? `${window.appState.baseUrl}api.php` : ''),
   params: {
-    access_token: process.env.VUE_APP_API_TOKEN || (document.querySelector('meta[name="token"]') ? document.querySelector('meta[name="token"]').content : '')
+    access_token: process.env.VUE_APP_API_TOKEN || (window.appState ? window.appState.token : '')
   }
 })
