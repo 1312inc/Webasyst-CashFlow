6002cc61ae1b61335f80d48ab98578d055f12860
Konstantin Zaytsev <zaytsev.const@gmail.com>
2020-09-23 22:47:09 +0300

    feat: create update delete categories and accounts

--- client/src/plugins/api.js
+++ client/src/plugins/api.js
@@ -4,5 +4,8 @@ export default axios.create({
   baseURL: process.env.VUE_APP_BASE_URL || (window.appState ? `${window.appState.baseUrl}api.php` : ''),
   params: {
     access_token: process.env.VUE_APP_API_TOKEN || (window.appState ? window.appState.token : '')
+  },
+  headers: {
+    'Content-Type': 'multipart/form-data'
   }
 })
