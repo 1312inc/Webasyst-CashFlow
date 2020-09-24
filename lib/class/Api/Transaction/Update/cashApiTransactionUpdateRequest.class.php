ea882f188c3600a9d87d57dfecfa244f962da22a
Kirill Maramygin <kirill.maramygin@gmail.com>
2020-09-25 00:07:20 +0300

    repeating transaction update fixes

--- lib/class/Api/Transaction/Update/cashApiTransactionUpdateRequest.class.php
+++ lib/class/Api/Transaction/Update/cashApiTransactionUpdateRequest.class.php
@@ -5,7 +5,7 @@
  *
  * repeating_frequency
  */
-class cashApiTransactionUpdateRequest extends cashApiTransactionCreateRequest
+class cashApiTransactionUpdateRequest
 {
     /**
      * @var int
@@ -16,4 +16,84 @@ class cashApiTransactionUpdateRequest extends cashApiTransactionCreateRequest
      * @var bool
      */
     public $apply_to_all_in_future = false;
+
+    /**
+     * @var float
+     */
+    public $amount = 0.0;
+
+    /**
+     * @var string
+     */
+    public $date = '';
+
+    /**
+     * @var int
+     */
+    public $account_id = 0;
+
+    /**
+     * @var int
+     */
+    public $category_id = 0;
+
+    /**
+     * @var int
+     */
+    public $contractor_contact_id;
+
+    /**
+     * New contractor name
+     *
+     * @var string
+     */
+    public $contractor;
+
+    /**
+     * @var string
+     */
+    public $description;
+
+    /**
+     * @var int
+     */
+    public $repeating_frequency;
+
+    /**
+     * One of 'day', 'week', 'month', 'year'.
+     *
+     * @see cashRepeatingTransaction
+     *
+     * @var string
+     */
+    public $repeating_interval;
+
+    /**
+     * One of 'after', 'ondate'.
+     *
+     * @see cashRepeatingTransaction
+     *
+     * @var string
+     */
+    public $repeating_end_type;
+
+    /**
+     * @var int
+     */
+    public $repeating_end_after;
+
+    /**
+     * @var string
+     */
+    public $repeating_end_ondate;
+
+    /**
+     * @var int
+     */
+    public $transfer_account_id;
+
+    /**
+     * @var string
+     */
+    public $transfer_incoming_amount;
 }
