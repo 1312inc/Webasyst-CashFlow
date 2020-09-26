b805c22423d0490d2f1da66b26303e2c3d2da9de
Konstantin Zaytsev <zaytsev.const@gmail.com>
2020-09-26 15:47:11 +0300

    fix: styles build file

--- client/vue.config.js
+++ client/vue.config.js
@@ -5,23 +5,26 @@ module.exports = {
   devServer: {
     proxy: 'http://localhost:8888'
   },
+
+  // configureWebpack: {
+  //   optimization: {
+  //     splitChunks: {
+  //       cacheGroups: {
+  //         default: false,
+  //         common: false,
+  //         styles: {
+  //           name: 'app',
+  //           test: /\.(s?css|vue)$/, // chunks plugin has to be aware of all kind of files able to output css in order to put them together
+  //           chunks: 'initial',
+  //           minChunks: 1,
+  //           enforce: true
+  //         }
+  //       }
+  //     }
+  //   }
+  // },
+
   chainWebpack: config => {
-    config
-      .plugin('prefetch')
-      .tap(args => {
-        return [
-          {
-            rel: 'prefetch',
-            include: 'asyncChunks',
-            fileBlacklist: [
-              /\.map$/,
-              /pdfmake\.[^.]+\.js$/,
-              /xlsx\.[^.]+\.js$/,
-              /fabric[^.]*\.[^.]+\.js$/,
-              /responsivedefaults\.[^.]+\.js$/
-            ]
-          }
-        ]
-      })
+    config.optimization.delete('splitChunks')
   }
 }
