3eae08f8d6967f18f7d43dc1c58f84758fbafda8
Konstantin Zaytsev <zaytsev.const@gmail.com>
2020-10-02 12:28:55 +0300

    build: separate desktop and mobile build process

---
+++ client/src/mobile.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import Helpers from './plugins/helpers'
+import Numeral from './plugins/numeralMoment'
+import Vuelidate from 'vuelidate'
+import vClickOutside from 'v-click-outside'
+import Noty from './plugins/noty'
+
+Vue.config.productionTip = false
+
+Vue.use(Numeral)
+Vue.use(Noty)
+Vue.use(Vuelidate)
+Vue.use(Helpers)
+Vue.use(vClickOutside)
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
--- client/vue.config.js
+++ client/vue.config.js
@@ -6,11 +6,9 @@ module.exports = {
   devServer: {
     proxy: 'http://localhost:8888'
   },
-
   configureWebpack: {
     plugins: [
       new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /en|ru/)
-      // new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
     ],
     externals: function (context, request, callback) {
       if (/xlsx|canvg|pdfmake/.test(request)) {
@@ -18,32 +16,7 @@ module.exports = {
       }
       callback()
     }
-
   },
-
-  // configureWebpack: {
-  //   optimization: {
-  //     splitChunks: {
-  //       cacheGroups: {
-  //         default: false,
-  //         common: false,
-  //         styles: {
-  //           name: 'app',
-  //           test: /\.(s?css|vue)$/, // chunks plugin has to be aware of all kind of files able to output css in order to put them together
-  //           chunks: 'initial',
-  //           minChunks: 1,
-  //           enforce: true
-  //         }
-  //       }
-  //     }
-  //   }
-  // },
-  // pluginOptions: {
-  //   webpackBundleAnalyzer: {
-  //     openAnalyzer: true
-  //   }
-  // },
-
   chainWebpack: config => {
     config.optimization.delete('splitChunks')
   }
