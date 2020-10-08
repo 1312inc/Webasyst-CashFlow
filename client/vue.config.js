12233c58846cbf8ccfda5131b4d1c3823bfb5c23
Konstantin Zaytsev <zaytsev.const@gmail.com>
2020-10-08 09:07:23 +0300

    build: separate desktop and monile environment

--- client/vue.config.js
+++ client/vue.config.js
@@ -19,5 +19,7 @@ module.exports = {
   },
   chainWebpack: config => {
     config.optimization.delete('splitChunks')
+    config.plugins.delete('preload')
+    config.plugins.delete('prefetch')
   }
 }
