a2c01d7c42990ec012f9d6db409ea1a0c0eb96c0
Konstantin Zaytsev <zaytsev.const@gmail.com>
2020-10-11 22:59:29 +0300

    7.445: implement base EN/RU localization @ SPA

--- client/src/desktop.js
+++ client/src/desktop.js
@@ -3,21 +3,36 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Helpers from './plugins/helpers'
+import VueI18n from 'vue-i18n'
 import Numeral from './plugins/numeralMoment'
 import Vuelidate from 'vuelidate'
 import vClickOutside from 'v-click-outside'
 import Noty from './plugins/noty'
 
+import en from './locales/en_US.json'
+import ru from './locales/ru_RU.json'
+
 Vue.config.productionTip = false
 
+Vue.use(VueI18n)
 Vue.use(Numeral)
 Vue.use(Noty)
 Vue.use(Vuelidate)
 Vue.use(Helpers)
 Vue.use(vClickOutside)
 
+const i18n = new VueI18n({
+  locale: window?.appState?.lang || 'en_US',
+  fallbackLocale: 'en_US',
+  messages: {
+    en_US: en,
+    ru_RU: ru
+  }
+})
+
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
