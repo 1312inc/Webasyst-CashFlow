2a5f6e4eab5fb174aa1a433ca34a1878eff91a18
Konstantin Zaytsev <zaytsev.const@gmail.com>
2020-10-19 12:26:48 +0300

    init feature

---
+++ client/src/Sidebar.vue
@@ -0,0 +1,27 @@
+<template>
+  <Sidebar />
+</template>
+
+<script>
+import Sidebar from '@/components/Sidebar'
+export default {
+  components: {
+    Sidebar
+  },
+
+  mounted () {
+    this.$store.dispatch('account/getList')
+    this.$store.dispatch('category/getList')
+  }
+}
+</script>
+
+<style lang="scss">
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+</style>
