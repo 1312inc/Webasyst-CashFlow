86426260dfd086a53de96695034fa828a61544dd
Kirill Maramygin <kirill.maramygin@gmail.com>
2020-10-30 15:30:29 +0300

    7.441:API method: cash.transactions.getShrinkedList

--- api/v1/cash.transaction.getShrinkList.method.php
+++ api/v1/cash.transaction.getShrinkList.method.php
@@ -1,80 +1,21 @@
 <?php
 
-/**
- * Class cashTransactionGetListMethod
- *
- * Request:
- * <code>
- * {
- *      "from": "2018-03-01",
- *      "to": "2020-04-01",
- *      "limit": "100",
- *      "start": "0",
- *      "filter": "category/2"
- * }
- * </code>
- *
- * from, to - date limits,
- * limit - how many transactions to get,
- * start - starting from,
- * filter - hash to filter transaction list, can be "category/X", "account/X", "contractor/X", "currency/XXX"
- *
- * all params are required
- *
- * Response:
- * <code>
- * [
- *  {
- *      "id": 2,
- *      "date": "2019-11-22",
- *      "datetime": "2019-11-22 00:00:00",
- *      "amount": 192000,
- *      "amountShorten": "192K",
- *      "balance": null,
- *      "balanceShorten": "0",
- *      "description": "Взяли кредит в банке",
- *      "repeating_id": null,
- *      "create_contact_id": 1,
- *      "create_datetime": "2020-10-22 20:33:34",
- *      "update_datetime": null,
- *      "category_id": 2,
- *      "account_id": 2,
- *      "planned": false,
- *      "is_archived": false,
- *      "external_hash": null,
- *      "external_source": null,
- *      "external_data": null,
- *      "contractor_contact_id": null
- *  }
- * ]
- * </code>
- */
 class cashTransactionGetShrinkListMethod extends cashApiAbstractMethod
 {
     protected $method = self::METHOD_GET;
 
     /**
-     * @return cashApiTransactionGetListResponse
-     * @throws kmwaForbiddenException
+     * @return cashApiTransactionGetShrinkListResponse
      * @throws waAPIException
      * @throws waException
      */
     public function run(): cashApiResponseInterface
     {
-        /** @var cashApiTransactionGetListRequest $request */
-        $request = $this->fillRequestWithParams(new cashApiTransactionGetListRequest());
-        $request->offset = (int) $request->offset;
-        if ($request->limit > 500) {
-            $request->limit = 500;
-        }
+        /** @var cashApiTransactionGetShrinkListRequest $request */
+        $request = $this->fillRequestWithParams(new cashApiTransactionGetShrinkListRequest());
 
-        $transactions = (new cashApiTransactionGetListHandler())->handle($request);
-
-        return new cashApiTransactionGetListResponse(
-            $transactions['data'],
-            $transactions['total'],
-            (int) $request->offset,
-            (int) $request->limit
+        return new cashApiTransactionGetShrinkListResponse(
+            (new cashApiTransactionGetShrinkListHandler())->handle($request)
         );
     }
 }
--- lib/class/Api/Transaction/GetShrinkList/cashApiTransactionGetShrinkListResponse.class.php
+++ lib/class/Api/Transaction/GetShrinkList/cashApiTransactionGetShrinkListResponse.class.php
@@ -9,18 +9,12 @@ class cashApiTransactionGetShrinkListResponse extends cashApiAbstractResponse
      * cashApiTransactionGetListResponse constructor.
      *
      * @param array|cashApiTransactionResponseDto[] $transactions
-     * @param int                                   $total
-     * @param int                                   $offset
-     * @param int                                   $limit
      */
-    public function __construct(array $transactions, $total, $offset, $limit)
+    public function __construct(array $transactions)
     {
         parent::__construct(200);
 
         $this->response = [
-            'offset' => $offset,
-            'limit' => $limit,
-            'total' => $total,
             'data' => $transactions,
         ];
     }
