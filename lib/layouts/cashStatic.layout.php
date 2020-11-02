ef86eb0424962857812fd01fe903fff3ef16dd5f
Kirill Maramygin <kirill.maramygin@gmail.com>
2020-11-02 14:08:30 +0300

    static layout token

--- lib/layouts/cashStatic.layout.php
+++ lib/layouts/cashStatic.layout.php
@@ -7,6 +7,8 @@ class cashStaticLayout extends waLayout
 {
     public function execute()
     {
+        $token = (new cashApiToken())->retrieveToken(cash()->getUser()->getContact());
 
+        $this->view->assign('token', $token);
     }
 }
