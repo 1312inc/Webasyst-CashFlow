0e0101d66ec8e4f2de6792d133429f7601ba076e
Kirill Maramygin <kirill.maramygin@gmail.com>
2020-11-07 11:20:18 +0300

    7.502:cash.aggregate.getBreakDown response format

--- lib/class/Api/Aggregate/GetBreakDown/cashApiAggregateGetBreakDownDataDto.class.php
+++ lib/class/Api/Aggregate/GetBreakDown/cashApiAggregateGetBreakDownDataDto.class.php
@@ -11,6 +11,16 @@ final class cashApiAggregateGetBreakDownDataDto
     public $amount;
 
     /**
+     * @var string
+     */
+    public $category_name;
+
+    /**
+     * @var string
+     */
+    public $category_color;
+
+    /**
      * @var int
      */
     public $category_id;
@@ -18,12 +28,16 @@ final class cashApiAggregateGetBreakDownDataDto
     /**
      * cashApiAggregateGetBreakDownDto constructor.
      *
-     * @param $amount
-     * @param $category_id
+     * @param                   $amount
+     * @param cashCategory|null $category
      */
-    public function __construct($amount, $category_id)
+    public function __construct($amount, ?cashCategory $category)
     {
         $this->amount = abs(round($amount, 2));
-        $this->category_id = (int) $category_id;
+        if ($category) {
+            $this->category_name = $category->getName();
+            $this->category_color = $category->getColor();
+            $this->category_id = $category->getId();
+        }
     }
 }
--- lib/class/Api/Aggregate/GetBreakDown/cashApiAggregateGetBreakDownDto.class.php
+++ lib/class/Api/Aggregate/GetBreakDown/cashApiAggregateGetBreakDownDto.class.php
@@ -6,16 +6,6 @@
 final class cashApiAggregateGetBreakDownDto
 {
     /**
-     * @var string
-     */
-    public $type;
-
-    /**
-     * @var string
-     */
-    public $currency;
-
-    /**
      * @var float
      */
     public $totalAmount = 0.0;
@@ -23,19 +13,5 @@ final class cashApiAggregateGetBreakDownDto
     /**
      * @var array|cashApiAggregateGetBreakDownDataDto[]
      */
-    public $data;
-
-    /**
-     * cashApiAggregateGetBreakDownDto constructor.
-     *
-     * @param string                                      $currency
-     * @param string                                      $type
-     * @param array|cashApiAggregateGetBreakDownDataDto[] $data
-     */
-    public function __construct(string $currency, string $type, array $data)
-    {
-        $this->currency = $currency;
-        $this->data = $data;
-        $this->type = $type;
-    }
+    public $data = [];
 }
