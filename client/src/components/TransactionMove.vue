90c554f193e867cdb1fc581d98da79c26bc3a645
Konstantin Zaytsev <zaytsev.const@gmail.com>
2020-11-11 12:40:53 +0300

    feat(7.501, 7.517): cash.transaction.bulkDelete & cash.transaction.bulkMove and Chart updates

---
+++ client/src/components/TransactionMove.vue
@@ -0,0 +1,154 @@
+<template>
+  <div>
+    <h2 class="custom-mb-32">{{ $t("moveTransactions", { count: count }) }}</h2>
+
+    <div class="fields custom-mb-32">
+      <div class="field">
+        <div class="name for-input">
+          {{ $t("account") }}
+        </div>
+        <div class="value">
+          <div class="wa-select">
+            <select
+              v-model="model.account_id"
+              :class="{ 'state-error': $v.model.account_id.$error }"
+            >
+              <option value="0">{{ $t("dontChange") }}</option>
+              <option
+                :value="account.id"
+                v-for="account in accounts"
+                :key="account.id"
+              >
+                {{ account.currency }} – {{ account.name }} ({{
+                  getCurrencySignByCode(account.currency)
+                }})
+              </option>
+            </select>
+          </div>
+        </div>
+      </div>
+
+      <div class="field">
+        <div class="name for-input">
+          {{ $t("category") }}
+        </div>
+        <div class="value">
+          <div class="wa-select">
+            <select
+              v-model="model.category_id"
+              :class="{ 'state-error': $v.model.category_id.$error }"
+            >
+              <option value="-1">{{ $t("dontChange") }}</option>
+              <option value="0">{{ $t("noCategory") }}</option>
+              <optgroup :label="$t('income')">
+                <option
+                  :value="category.id"
+                  v-for="category in categoriesIncome"
+                  :key="category.id"
+                >
+                  {{ category.name }}
+                </option>
+              </optgroup>
+              <optgroup :label="$t('expense')">
+                <option
+                  :value="category.id"
+                  v-for="category in categoriesExpense"
+                  :key="category.id"
+                >
+                  {{ category.name }}
+                </option>
+              </optgroup>
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <span class="smaller alert warning custom-mb-24">
+      <i class="fas fa-exclamation-triangle"></i>
+      {{ $t("bulkMoveWarning") }}
+    </span>
+
+    <div class="flexbox">
+      <div class="flexbox space-1rem wide">
+        <button @click="submit" class="button purple">
+          {{ $t("updateTransactions", { count: count }) }}
+        </button>
+        <button @click="close" class="button light-gray">
+          {{ $t("cancel") }}
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters } from 'vuex'
+import { required } from 'vuelidate/lib/validators'
+export default {
+  props: {
+    ids: {
+      type: Array,
+      required: true
+    }
+  },
+
+  data () {
+    return {
+      model: {
+        ids: this.ids,
+        account_id: 0,
+        category_id: -1
+      }
+    }
+  },
+
+  validations: {
+    model: {
+      account_id: {
+        required
+      },
+      category_id: {
+        required
+      }
+    }
+  },
+
+  computed: {
+    ...mapState('account', ['accounts']),
+    ...mapState('category', ['categories']),
+    ...mapGetters('system', ['getCurrencySignByCode']),
+
+    categoriesIncome () {
+      return this.categories.filter((c) => c.type === 'income')
+    },
+
+    categoriesExpense () {
+      return this.categories.filter((c) => c.type === 'expense')
+    },
+
+    count () {
+      return this.ids.length
+    }
+  },
+
+  methods: {
+    submit () {
+      this.$v.$touch()
+      if (!this.$v.$invalid) {
+        this.$store
+          .dispatch('transaction/bulkMove', this.model)
+          .then(() => {
+            this.$emit('success')
+            this.close()
+          })
+          .catch(() => {})
+      }
+    },
+
+    close () {
+      this.$parent.$emit('close')
+    }
+  }
+}
+</script>
