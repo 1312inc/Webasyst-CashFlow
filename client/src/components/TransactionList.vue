6a4abad04dc0ba17858947f09bef668089541ef7
Konstantin Zaytsev <zaytsev.const@gmail.com>
2020-11-24 17:59:31 +0300

    7.534: grouping @ transaction list

--- client/src/components/TransactionList.vue
+++ client/src/components/TransactionList.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
 
-    <div v-if="!loading" class="flexbox custom-mb-16">
+    <div v-if="!loading" class="flexbox custom-mb-24">
       <div v-if="checkedRows.length" class="flexbox space-1rem middle wide">
         <button @click="openMove = true" class="yellow red"><i class="fas fa-arrow-right"></i> {{ $t('move') }} {{ checkedRows.length }}</button>
         <button @click="bulkDelete" class="button red"><i class="fas fa-trash-alt"></i> {{ $t('delete') }} {{ checkedRows.length }}</button>
@@ -63,31 +63,101 @@
 
     <transition name="fade-appear">
       <div v-if="!loading">
-        <table
-          class="small zebra"
-          v-if="transactions.data.length"
-        >
-          <tr>
-            <th class="min-width tw-border-0 tw-border-b tw-border-solid tw-border-gray-400">
-              <input type="checkbox" @click="checkAll" />
-            </th>
-            <th
-              colspan="5"
-              class="tw-border-0 tw-border-b tw-border-solid tw-border-gray-400"
+        <div v-if="isTransactionsExists">
+          <div v-if="transactions.offset === 0" class="custom-mb-24"> <!-- If firts page -->
+            <div class="flexbox middle">
+              <div @click="toggleupcomingBlockOpened" class="tw-cursor-pointer custom-mr-8 black" :class="{'tw-opacity-50': !upcomingBlockOpened}">
+                <i class="fas fa-caret-down"></i>&nbsp;
+                <span v-if="featurePeriod === 1">{{ $t('tomorrow') }}</span>
+                <span v-else>{{ $t('nextDays', {'count': featurePeriod}) }}</span>&nbsp;
+                <span v-if="upcomingTransactions.length">({{ upcomingTransactions.length }})</span>
+              </div>
+              <div @mouseover="$refs.dropdown.style.display = 'block'" @mouseleave="$refs.dropdown.style.display = 'none'" class="dropdown">
+                <i class="fas fa-ellipsis-h"></i>
+                <div class="dropdown-body" ref="dropdown">
+                    <ul class="menu-v">
+                        <li>
+                            <a href="#" @click.prevent="setFeaturePeriod(1)"><span>{{ $t('tomorrow') }}</span></a>
+                        </li>
+                        <li>
+                            <a href="#" @click.prevent="setFeaturePeriod(7)"><span>{{ $t('nextDays', {'count': 7}) }}</span></a>
+                        </li>
+                        <li>
+                            <a href="#" @click.prevent="setFeaturePeriod(30)"><span>{{ $t('nextDays', {'count': 30}) }}</span></a>
+                        </li>
+                    </ul>
+                </div>
+              </div>
+            </div>
+            <div v-if="upcomingBlockOpened">
+              <table
+                class="small zebra custom-mt-12"
+                v-if="upcomingTransactions.length"
+              >
+                <tr>
+                  <th class="min-width tw-border-0 tw-border-b tw-border-solid tw-border-gray-400">
+                    <!-- <input type="checkbox" @click="checkAll" /> -->
+                  </th>
+                  <th
+                    colspan="5"
+                    class="tw-border-0 tw-border-b tw-border-solid tw-border-gray-400"
+                  >
+                    {{
+                      $t("transactionsListCount", { count: upcomingTransactions.length })
+                    }}
+                  </th>
+                </tr>
+                <TransactionListRow
+                  v-for="transaction in upcomingTransactions"
+                  :key="transaction.id"
+                  :transaction="transaction"
+                  :is-checked="checkedRows.includes(transaction.id)"
+                  @checkboxUpdate="onTransactionListRowUpdate(transaction.id)"
+                />
+              </table>
+              <div v-else class="tw-text-center custom-py-20">
+                {{ $t('emptyListUpcoming') }}
+              </div>
+            </div>
+          </div>
+
+          <div v-for="(transitions, period) in incomingTransactions" :key="period">
+            <div v-if="period === 'today'" class="black">
+              {{ $t('today') }}
+            </div>
+            <div v-else class="black">
+              {{ $moment(period).format('MMMM, YYYY') }}
+            </div>
+            <table
+              class="small zebra custom-mb-24"
+              v-if="transitions.length"
             >
-              {{
-                $t("transactionsListCount", { count: transactions.total })
-              }}
-            </th>
-          </tr>
-          <TransactionListRow
-            v-for="transaction in transactions.data"
-            :key="transaction.id"
-            :transaction="transaction"
-            :is-checked="checkedRows.includes(transaction.id)"
-            @checkboxUpdate="onTransactionListRowUpdate(transaction.id)"
-          />
-        </table>
+              <tr>
+                <th class="min-width tw-border-0 tw-border-b tw-border-solid tw-border-gray-400">
+                  <!-- <input type="checkbox" @click="checkAll" /> -->
+                </th>
+                <th
+                  colspan="5"
+                  class="tw-border-0 tw-border-b tw-border-solid tw-border-gray-400"
+                >
+                  {{
+                    $t("transactionsListCount", { count: transitions.length })
+                  }}
+                </th>
+              </tr>
+              <TransactionListRow
+                v-for="transaction in transitions"
+                :key="transaction.id"
+                :transaction="transaction"
+                :is-checked="checkedRows.includes(transaction.id)"
+                @checkboxUpdate="onTransactionListRowUpdate(transaction.id)"
+              />
+            </table>
+            <div v-else class="tw-text-center custom-py-20">
+              {{ $t('emptyListToday') }}
+            </div>
+          </div>
+        </div>
         <div v-else class="tw-text-center custom-py-20">
           {{ $t('emptyList') }}
         </div>
@@ -118,7 +188,9 @@ export default {
       open: false,
       categoryType: '',
       openMove: false,
-      checkedRows: []
+      checkedRows: [],
+      featurePeriod: 7,
+      upcomingBlockOpened: false
     }
   },
 
@@ -134,7 +206,66 @@ export default {
     ...mapState('transaction', ['transactions', 'loading']),
     ...mapGetters({
       currentType: 'getCurrentType'
-    })
+    }),
+
+    sortedTransactions () {
+      const today = this.$moment().format('YYYY-MM-DD')
+      const result = this.transactions.data.reduce((arr, t) => {
+        const istart = this.$moment(t.date)
+        if (istart.diff(today, 'days') > 0) {
+          arr.upcoming.push(t)
+        } else {
+          arr.incoming.push(t)
+        }
+        return arr
+      }, {
+        incoming: [],
+        upcoming: []
+      })
+      return result
+    },
+
+    isTransactionsExists () {
+      return this.sortedTransactions.incoming.length || this.sortedTransactions.upcoming.length
+    },
+
+    incomingTransactions () {
+      const today = this.$moment().format('YYYY-MM-DD')
+      const acc = this.transactions.offset === 0 ? { today: [] } : {}
+      const result = this.sortedTransactions.incoming.reduce((acc, e) => {
+        const month = this.$moment(e.date).format('YYYY-MM')
+        if (e.date === today && acc.today) {
+          acc.today.push(e)
+          return acc
+        }
+        if (month in acc) {
+          acc[month].push(e)
+        } else {
+          acc[month] = [e]
+        }
+        return acc
+      }, acc)
+
+      return result
+    },
+
+    upcomingTransactions () {
+      const today = this.$moment().format('YYYY-MM-DD')
+      const result = this.sortedTransactions.upcoming.reduce((arr, t) => {
+        const istart = this.$moment(t.date)
+        if (istart.diff(today, 'days') <= this.featurePeriod) {
+          arr.push(t)
+        }
+        return arr
+      }, [])
+      return result
+    }
+
+  },
+
+  created () {
+    this.featurePeriod = +localStorage.getItem('upcoming_transactions_days') || this.featurePeriod
+    this.upcomingBlockOpened = +localStorage.getItem('upcoming_transactions_show') || this.upcomingBlockOpened
   },
 
   methods: {
@@ -164,6 +295,18 @@ export default {
           })
           .catch(() => {})
       }
+    },
+
+    setFeaturePeriod (days) {
+      this.featurePeriod = days
+      this.upcomingBlockOpened = true
+      localStorage.setItem('upcoming_transactions_days', days)
+      localStorage.setItem('upcoming_transactions_show', 1)
+    },
+
+    toggleupcomingBlockOpened () {
+      this.upcomingBlockOpened = !this.upcomingBlockOpened
+      localStorage.setItem('upcoming_transactions_show', this.upcomingBlockOpened ? 1 : 0)
     }
   }
 }
