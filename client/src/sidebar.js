b17338356200dde29c31338f6097e9ac688d7fe2
Konstantin Zaytsev <zaytsev.const@gmail.com>
2020-11-26 11:12:55 +0300

    update: some transition list and chart updates

--- client/src/sidebar.js
+++ client/src/sidebar.js
@@ -1,17 +1,17 @@
 import Vue from 'vue'
 import App from './Sidebar.vue'
 import store from './store'
+import permissions from './plugins/permissions'
 import Helpers from './plugins/helpers'
 import SidebarRouterFallback from './plugins/sidebarRouterFallback'
 import { i18n } from './plugins/locale'
-// import Numeral from './plugins/numeralMoment'
 import Vuelidate from 'vuelidate'
 import './assets/styles/tailwindcss.css'
 import './assets/styles/transitions.css'
 
 Vue.config.productionTip = false
 
-// Vue.use(Numeral)
+Vue.use(permissions)
 Vue.use(Vuelidate)
 Vue.use(Helpers)
 Vue.use(SidebarRouterFallback)
