4f20934f52105619efc0db4365d35e5424734620
Kirill Maramygin <kirill.maramygin@gmail.com>
2021-01-13 08:37:30 +0300

    7.565:display latest import list @ 'Import' screen

--- lib/actions/import/cashImport.action.php
+++ lib/actions/import/cashImport.action.php
@@ -21,5 +21,12 @@ class cashImportAction extends cashViewAction
      */
     public function runAction($params = null)
     {
+        $importDtos = [];
+        if (cash()->getUser()->canImport()) {
+            $imports = cash()->getEntityRepository(cashImport::class)->findLastN(10);
+            $importDtos = cashDtoFromEntityFactory::fromEntities(cashImportDto::class, $imports);
+        }
+
+        $this->view->assign(['imports' => $importDtos]);
     }
 }
