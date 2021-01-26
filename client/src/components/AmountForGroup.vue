4df7cef1f4c5619e4f18490f40a690f0526058ee
Konstantin Zaytsev <zaytsev.const@gmail.com>
2021-01-26 16:08:00 +0300

    feat: calculating total for group with multiple currencies

--- client/src/components/AmountForGroup.vue
+++ client/src/components/AmountForGroup.vue
@@ -1,19 +1,29 @@
 <template>
-  <div
-    :class="{
-      'text-green': type === 'income',
-      'text-red': type === 'expense',
-    }"
-  >
-    <div>
-      <i
-        class="fas"
-        :class="{
-          'fa-caret-up': type === 'income',
-          'fa-caret-down': type === 'expense',
-        }"
-      ></i
-      >&nbsp;<span class="small">{{ $helper.toCurrency(total, null, true) }}</span>
+  <div class="flexbox space-12">
+    <div
+      v-for="account in accounts"
+      :key="account.id"
+      :class="{
+        'text-green': type === 'income',
+        'text-red': type === 'expense',
+      }"
+    >
+      <div>
+        <i
+          class="fas"
+          :class="{
+            'fa-caret-up': type === 'income',
+            'fa-caret-down': type === 'expense',
+          }"
+        ></i
+        >&nbsp;<span class="small">{{
+          $helper.toCurrency(
+            getTotalByAccout(account.id),
+            account.currency,
+            true
+          )
+        }}</span>
+      </div>
     </div>
   </div>
 </template>
@@ -33,9 +43,25 @@ export default {
   },
 
   computed: {
-    total () {
+    accounts () {
+      return this.group.reduce((acc, e) => {
+        const account = this.$store.getters['account/getById'](e.account_id)
+        if (account && !acc.includes(account)) {
+          acc.push(account)
+        }
+        return acc
+      }, [])
+    }
+  },
+
+  methods: {
+    getTotalByAccout (accountId) {
       return this.group
-        .filter(e => (this.type === 'income' ? e.amount >= 0 : e.amount < 0))
+        .filter(
+          e =>
+            (this.type === 'income' ? e.amount >= 0 : e.amount < 0) &&
+            e.account_id === accountId
+        )
         .reduce((acc, e) => {
           return acc + e.amount
         }, 0)
