aafad48c9e4ae3b25095158438c2d1107dfa3904
Kirill Maramygin <kirill.maramygin@gmail.com>
2021-01-31 18:24:19 +0300

    7.605:avoid creating multiple starter accounts

--- lib/config/install.php
+++ lib/config/install.php
@@ -1,5 +1,7 @@
 <?php
 
+cash()->getLogger()->log('Cash install start', 'install');
+
 $m = new waModel();
 $db = require wa('cash')->getConfig()->getAppConfigPath('db');
 
@@ -8,8 +10,8 @@ foreach ($db as $table => $info) {
     try {
         $m->exec(sprintf('alter table %s engine=innodb', $table));
         $innodb = 1;
-    } catch (waException $ex) {
-        cash()->getLogger()->error('mysql do not support InnoDb engine', $ex);
+    } catch (Exception $ex) {
+        cash()->getLogger()->error('mysql do not support InnoDb engine', $ex, 'install');
     } finally {
         (new waAppSettingsModel())->set('cash', 'innodb', $innodb);
     }
@@ -47,14 +49,21 @@ if ($innodb) {
                         foreign key (category_id) references cash_category (id)
                             on update cascade on delete set null'
         );
+
+        cash()->getLogger()->log('Foreign key added', 'install');
+
+        $installing = new cashFixtures();
+
+        cash()->getLogger()->log('Start: add accounts and categories', 'install');
+        $installing->createAccountsAndCategories();
+        cash()->getLogger()->log('Done: add accounts and categories', 'install');
+
+        cash()->getLogger()->log('Start: create demo account', 'install');
+        $installing->createDemo();
+        cash()->getLogger()->log('Done: create demo account', 'install');
     } catch (waException $ex) {
-        cash()->getLogger()->error('fail to add foreign keys', $ex);
+        cash()->getLogger()->error('Fail on add foreign keys or account/categories creation', $ex);
     }
 } else {
     throw new waException('InnoDb engine is required');
 }
-
-$installing = new cashFixtures();
-
-$installing->createAccountsAndCategories();
-$installing->createDemo();
