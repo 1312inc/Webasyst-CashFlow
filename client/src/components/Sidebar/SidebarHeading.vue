476b18e734c476906cd05a1d6325efaf1b88d257
Konstantin Zaytsev <zaytsev.const@gmail.com>
2021-02-04 21:36:28 +0300

    update(Sidebar): Explode headings and footer as components

---
+++ client/src/components/Sidebar/SidebarHeading.vue
@@ -0,0 +1,34 @@
+<template>
+  <h6 class="heading">
+    <span><slot></slot></span>
+    <a @click="openModal = true" class="count">
+      <i class="fas fa-plus-circle"></i>
+    </a>
+    <portal>
+      <Modal v-if="openModal" @close="openModal = false">
+        <component :is="updatingEntityName"></component>
+      </Modal>
+    </portal>
+  </h6>
+</template>
+
+<script>
+import Modal from '@/components/Modal'
+import Account from '@/components/AddAccount'
+import Category from '@/components/AddCategory'
+export default {
+  props: ['updatingEntityName'],
+
+  components: {
+    Modal,
+    Account,
+    Category
+  },
+
+  data () {
+    return {
+      openModal: false
+    }
+  }
+}
+</script>
