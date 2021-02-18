71f8923751923381e0215581ccb8fbf9f9a781f5
Konstantin Zaytsev <zaytsev.const@gmail.com>
2021-02-18 23:34:19 +0300

    build: mobile build

--- client/src/Mobile.vue
+++ client/src/Mobile.vue
@@ -38,24 +38,6 @@ export default {
     window.eventBus.$on('openDialog', (type = 'Category', editedItem = null) => {
       this.update(type, editedItem)
     })
-
-    // await this.$store.dispatch('system/getCurrencies')
-    // await Promise.all([
-    //   this.$store.dispatch('account/getList'),
-    //   this.$store.dispatch('category/getList')
-    // ])
-
-    const from = this.getDate(
-      'from',
-      this.$moment().add(-1, 'Y').format('YYYY-MM-DD')
-    )
-
-    const to = this.getDate(
-      'to',
-      this.$moment().add(6, 'M').format('YYYY-MM-DD')
-    )
-
-    this.$store.commit('transaction/updateQueryParams', { from, to })
   },
 
   methods: {
@@ -67,16 +49,8 @@ export default {
 
     close () {
       window.android.goBack()
-    },
-
-    getDate (type, defaultDate) {
-      let result = defaultDate
-      const lsValue = localStorage.getItem(`interval_${type}`)
-      if (lsValue) {
-        result = this.$store.state.intervals[type].find((e) => e.key === lsValue)?.value || defaultDate
-      }
-      return result
     }
+
   }
 }
 </script>
