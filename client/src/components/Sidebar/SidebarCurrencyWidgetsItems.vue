3fe12bd0c80b6e9589eb4e048f4bc2be38ce7e9c
Konstantin Zaytsev <zaytsev.const@gmail.com>
2021-02-24 13:01:21 +0300

    update(7.673): currency widgets

---
+++ client/src/components/Sidebar/SidebarCurrencyWidgetsItems.vue
@@ -0,0 +1,79 @@
+<template>
+  <li>
+    <router-link
+      :to="`/currency/${balanceFlow.currency}`"
+      style="display: block"
+    >
+      <div class="flexbox middle">
+        <div
+          :class="{ 'text-red': balanceFlow.balances.now.amount < 0 }"
+          class="wide large bold nowrap"
+        >
+          {{
+            $helper.toCurrency({
+              value: balanceFlow.balances.now.amount,
+              currencyCode: balanceFlow.currency,
+            })
+          }}
+        </div>
+        <div v-if="alertDate" class="custom-ml-4">
+          <span
+            :class="
+              alertDate.amount >= 0 ? 'c-bwc-badge--green' : 'c-bwc-badge--red'
+            "
+            class="c-bwc-badge small nowrap"
+            ><i
+              :class="
+                alertDate.amount >= 0 ? 'fa-check' : 'fa-exclamation-triangle'
+              "
+              class="fas custom-mr-4"
+              style="color: white"
+            ></i
+            >{{ $moment(alertDate.period).format("MMM Do") }}</span
+          >
+        </div>
+      </div>
+      <CurrencyChart :currency="balanceFlow" />
+    </router-link>
+  </li>
+</template>
+
+<script>
+import CurrencyChart from '@/components/CurrencyChart'
+export default {
+  props: ['balanceFlow'],
+
+  components: {
+    CurrencyChart
+  },
+
+  computed: {
+    alertDate () {
+      const now = this.balanceFlow.balances.now.date
+      const nowAmount = this.balanceFlow.balances.now.amount
+      return this.balanceFlow.data.find(element => {
+        return (
+          element.period > now &&
+          Math.sign(nowAmount) !== Math.sign(element.amount)
+        )
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.c-bwc-badge {
+  color: #fff;
+  padding: 2px 6px;
+  border-radius: 4px;
+
+  &--green {
+    background: var(--green);
+  }
+
+  &--red {
+    background: var(--red);
+  }
+}
+</style>
