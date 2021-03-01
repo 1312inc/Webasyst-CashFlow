140eb82df09e32f353da72901b6dcd69edce55fb
Konstantin Zaytsev <zaytsev.const@gmail.com>
2021-03-01 17:04:45 +0300

    update: balanceFlow chart & contractor component

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,4 +1,11 @@
+import { i18n } from '@/plugins/locale'
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
+
+export const decorators = [(story) => ({
+  components: { story },
+  template: '<story />',
+  i18n
+})];
