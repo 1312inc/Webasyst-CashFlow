9516be045cf64be1d5116b928bdf0a017d5dbdc3
Kirill Maramygin <kirill.maramygin@gmail.com>
2021-03-24 08:08:35 +0300

    dds report view

--- lib/actions/report-legacy/cashReport.action.php
+++ lib/actions/report-legacy/cashReport.action.php
@@ -5,15 +5,6 @@
  */
 class cashReportAction extends cashViewAction
 {
-    public function preExecute()
-    {
-        if (wa()->whichUI() === '2.0') {
-            $this->setLayout(new cashStaticLayout());
-        }
-
-        parent::preExecute();
-    }
-
     /**
      * @param null $params
      *
