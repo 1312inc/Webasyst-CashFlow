b2f5f3114a03558ea9caebff55ffa619a9b30301
Kirill Maramygin <kirill.maramygin@gmail.com>
2021-03-30 21:49:42 +0300

    7.706:Implement apply_to_all_in_future param to the cash.transaction.delete method

--- lib/class/Api/Transaction/Delete/cashApiTransactionDeleteResponse.class.php
+++ lib/class/Api/Transaction/Delete/cashApiTransactionDeleteResponse.class.php
@@ -5,4 +5,10 @@
  */
 class cashApiTransactionDeleteResponse extends cashApiAbstractResponse
 {
+    public function __construct(array $deletedIds)
+    {
+        parent::__construct(200);
+
+        $this->response = $deletedIds;
+    }
 }
