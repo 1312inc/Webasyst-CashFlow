89497d1b893c4df0a8467eb5bb4be524cec0ee28
Konstantin Zaytsev <zaytsev.const@gmail.com>
2021-04-19 21:40:58 +0300

    feat(7.824): 'In N days' .badge.squared for upcoming transactions

--- client/src/components/TransactionList/TransactionListGroupRow/TransactionListGroupRowDesc.vue
+++ client/src/components/TransactionList/TransactionListGroupRow/TransactionListGroupRowDesc.vue
@@ -3,6 +3,16 @@
     class="flexbox middle space-4 semibold custom-mb-4"
     style="overflow: hidden"
   >
+    <span
+      v-if="daysBefore > 0 && daysBefore < 6"
+      class="badge squared light-gray small"
+      style="text-transform: capitalize"
+      >{{
+        daysBefore === 1
+          ? $t("tomorrow")
+          : this.$moment(transaction.date).from($helper.currentDate)
+      }}</span
+    >
     <!-- if collapsed transactions header -->
     <template v-if="collapseHeaderData">
       <div
@@ -71,6 +81,15 @@
 
 <script>
 export default {
-  props: ['transaction', 'collapseHeaderData', 'isRepeatingGroup', 'category']
+  props: ['transaction', 'collapseHeaderData', 'isRepeatingGroup', 'category'],
+
+  computed: {
+    daysBefore () {
+      return this.$moment(this.transaction.date).diff(
+        this.$helper.currentDate,
+        'days'
+      )
+    }
+  }
 }
 </script>
