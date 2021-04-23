c5c1fa6e38d846a060bc0a89b8dfa4bb7c471e3c
Kirill Maramygin <kirill.maramygin@gmail.com>
2021-04-23 17:54:47 +0300

    7.845:Cash app won't delete via Installer

--- lib/config/uninstall.php
+++ lib/config/uninstall.php
@@ -6,27 +6,25 @@ $m = new waModel();
 $db = require wa('cash')->getConfig()->getAppConfigPath('db');
 
 $innodb = 1;
-foreach ($db as $table => $info) {
-    try {
-        waLog::log('disable all fk checks', 'cash/uninstall.log');
-        $m->exec('SET FOREIGN_KEY_CHECKS = 0');
-        $innodb = 1;
+try {
+    waLog::log('disable all fk checks', 'cash/uninstall.log');
+    $m->exec('SET FOREIGN_KEY_CHECKS = 0');
+    $innodb = 1;
 
-        foreach ($db as $tableName => $tableInfo) {
-            waLog::log(sprintf('disable fk checks for table %s', $tableName), 'cash/uninstall.log');
-            $m->exec(sprintf('ALTER TABLE %s DISABLE KEYS', $tableName));
+    foreach ($db as $tableName => $tableInfo) {
+        waLog::log(sprintf('disable fk checks for table %s', $tableName), 'cash/uninstall.log');
+        $m->exec(sprintf('ALTER TABLE %s DISABLE KEYS', $tableName));
 
-            waLog::log(sprintf('truncate table %s', $tableName), 'cash/uninstall.log');
-            $m->exec('truncate table ' . $tableName);
-        }
-    } catch (Exception $ex) {
-        waLog::log($ex->getMessage());
-        waLog::log($ex->getTraceAsString());
-    } finally {
-        if ($innodb) {
-            waLog::log('enable all fk checks', 'cash/uninstall.log');
-            $m->exec('SET FOREIGN_KEY_CHECKS = 1');
-        }
+        waLog::log(sprintf('truncate table %s', $tableName), 'cash/uninstall.log');
+        $m->exec('DROP TABLE IF EXISTS ' . $tableName);
+    }
+} catch (Exception $ex) {
+    waLog::log($ex->getMessage());
+    waLog::log($ex->getTraceAsString());
+} finally {
+    if ($innodb) {
+        waLog::log('enable all fk checks', 'cash/uninstall.log');
+        $m->exec('SET FOREIGN_KEY_CHECKS = 1');
     }
 }
 
