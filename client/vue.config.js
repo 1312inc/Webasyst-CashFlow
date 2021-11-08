e26c0bdf67f6106231b58f25f20cb41fe914a42a
Konstantin Zaytsev <zaytsev.const@gmail.com>
2021-11-08 15:22:42 +0300

    update depts

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,30 +1,31 @@
 var webpack = require('webpack')
-var { WebpackManifestPlugin } = require('webpack-manifest-plugin')
 var fetch = require('node-fetch')
 
-fetch(process.env.VUE_APP_DEV_PROXY + '/api.php/cash.system.getCurrencies?access_token=' + process.env.VUE_APP_API_TOKEN)
-  .then(response => response.text())
-  .then(res => {
-    process.env.VUE_APP_CURRENCIES = res
-  })
+if (process.env.NODE_ENV === 'development') {
+  fetch(process.env.VUE_APP_DEV_PROXY + '/api.php/cash.system.getCurrencies?access_token=' + process.env.VUE_APP_API_TOKEN)
+    .then(response => response.text())
+    .then(res => {
+      process.env.VUE_APP_CURRENCIES = res
+    })
 
-fetch(process.env.VUE_APP_DEV_PROXY + '/api.php/cash.account.getList?access_token=' + process.env.VUE_APP_API_TOKEN)
-  .then(response => response.text())
-  .then(res => {
-    process.env.VUE_APP_ACCOUNTS = res
-  })
+  fetch(process.env.VUE_APP_DEV_PROXY + '/api.php/cash.account.getList?access_token=' + process.env.VUE_APP_API_TOKEN)
+    .then(response => response.text())
+    .then(res => {
+      process.env.VUE_APP_ACCOUNTS = res
+    })
 
-fetch(process.env.VUE_APP_DEV_PROXY + '/api.php/cash.category.getList?access_token=' + process.env.VUE_APP_API_TOKEN)
-  .then(response => response.text())
-  .then(res => {
-    process.env.VUE_APP_CATEGORIES = res
-  })
+  fetch(process.env.VUE_APP_DEV_PROXY + '/api.php/cash.category.getList?access_token=' + process.env.VUE_APP_API_TOKEN)
+    .then(response => response.text())
+    .then(res => {
+      process.env.VUE_APP_CATEGORIES = res
+    })
 
-fetch(process.env.VUE_APP_DEV_PROXY + '/api.php/cash.system.getSettings?access_token=' + process.env.VUE_APP_API_TOKEN)
-  .then(response => response.text())
-  .then(res => {
-    process.env.VUE_APP_SETTINGS = res
-  })
+  fetch(process.env.VUE_APP_DEV_PROXY + '/api.php/cash.system.getSettings?access_token=' + process.env.VUE_APP_API_TOKEN)
+    .then(response => response.text())
+    .then(res => {
+      process.env.VUE_APP_SETTINGS = res
+    })
+}
 
 module.exports = {
   productionSourceMap: false,
@@ -44,12 +45,7 @@ module.exports = {
   },
   configureWebpack: {
     plugins: [
-      new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /en|ru/),
-      new WebpackManifestPlugin({
-        seed: {
-          version: process.env.npm_package_version
-        }
-      })
+      new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /en|ru/)
     ],
     externals: function (context, request, callback) {
       if (/xlsx|canvg|pdfmake/.test(request)) {
