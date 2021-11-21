968b107500691902c8188008dcb5c875e187210b
Konstantin Zaytsev <zaytsev.const@gmail.com>
2021-11-21 16:01:09 +0300

    7.944: quick bulk transaction add dialog

--- client/src/store/modules/transactionBulk.js
+++ client/src/store/modules/transactionBulk.js
@@ -44,6 +44,20 @@ export default {
   },
 
   actions: {
+    async bulkCreate ({ commit }, transactions) {
+      try {
+        const { data: createdTransactions } = await api.post(
+          'cash.transaction.bulkCreate',
+          transactions
+        )
+        commit('transaction/createTransactions', createdTransactions, {
+          root: true
+        })
+      } catch (_) {
+        return false
+      }
+    },
+
     async bulkDelete ({ dispatch, state, commit }) {
       const ids = state.selectedTransactionsIds
       try {
