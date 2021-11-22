1d0231c98909a0add0b56eeb8cd9c8dfaefb33f9
Kirill Maramygin <kirill.maramygin@gmail.com>
2021-11-22 22:11:51 +0300

    7.960:bulkComplete API method to automatically set transaction date on today

--- lib/class/Api/Transaction/BulkComplete/cashApiTransactionBulkCompleteRequest.class.php
+++ lib/class/Api/Transaction/BulkComplete/cashApiTransactionBulkCompleteRequest.class.php
@@ -1,12 +1,37 @@
 <?php
 
-/**
- * Class cashApiTransactionBulkCompleteRequest
- */
-class cashApiTransactionBulkCompleteRequest
+final class cashApiTransactionBulkCompleteRequest
 {
+    private const MAX_IDS = 500;
+
     /**
      * @var array<int>
      */
-    public $ids = [];
+    private $ids;
+
+    /**
+     * @param int[] $ids
+     */
+    public function __construct(array $ids) {
+        if (count($ids) > self::MAX_IDS) {
+            throw new cashValidateException(
+                sprintf_wp('Too many transactions to move. Max limit is %d', self::MAX_IDS)
+            );
+        }
+
+        array_walk($ids, 'intval');
+        $ids = array_filter($ids, static function ($id) {
+            return $id > 0;
+        });
+
+        $this->ids = $ids;
+    }
+
+    /**
+     * @return int[]
+     */
+    public function getIds(): array
+    {
+        return $this->ids;
+    }
 }
