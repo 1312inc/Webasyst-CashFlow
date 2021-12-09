a189730496d0ad5633cd95b5acb24e8d178bbab5
Konstantin Zaytsev <zaytsev.const@gmail.com>
2021-12-09 21:21:58 +0300

    7.991:h1 current balance: display the 'average speed' in case not 0

---
+++ client/src/components/ChartHeaderTitleAverage.vue
@@ -0,0 +1,45 @@
+<template>
+  <div
+    v-if="average !== 0 && !this.$store.state.transaction.loadingChart"
+    :class="average >= 0 ? 'text-green' : 'text-red'"
+    :title="$t('forecasted')"
+    class="opacity-50"
+  >
+    <i class="fas fa-chart-line" />
+    ≈
+    {{
+      $helper.toCurrency({
+        value: average,
+        currencyCode: currencyCode,
+        isDynamics: true
+      })
+    }}/{{ $t("shortMonth") }}
+  </div>
+</template>
+
+<script>
+import amountMixin from '@/mixins/amountMixin'
+export default {
+  props: {
+    currencyCode: {
+      type: String
+    }
+  },
+  mixins: [amountMixin],
+  computed: {
+    average () {
+      const amount = this.$_amountMixin_amountDelta('to')
+      const months = Math.ceil(
+        Math.abs(
+          this.$moment().diff(
+            this.$moment(this.$store.state.transaction.chartInterval.to),
+            'months',
+            true
+          )
+        )
+      )
+      return amount / months
+    }
+  }
+}
+</script>
