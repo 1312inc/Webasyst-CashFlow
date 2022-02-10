5c2a7641652bdb478073537c536f6aee921b902c
Kirill Maramygin <kirill.maramygin@gmail.com>
2022-02-10 08:54:16 +0300

    7.1055: create a simple 'helloworld' WA plugin prototype integrated into the Cash SPA

---
+++ plugins/charttoggle/lib/config/plugin.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'name' => 'Charttoggle',
+    'img' => 'img/charttoggle.gif',
+    'version' => '0.0.1',
+    'vendor' => '--',
+    'handlers' =>
+        [
+            'backend_layout' => 'handleBackendLayoutHook',
+        ],
+];
