1c31947cbcaa127717597b303caf27381833c9af
Kirill Maramygin <kirill.maramygin@gmail.com>
2022-02-17 20:54:50 +0300

    7.1064: Api: Order Info method

--- lib/class/Api/System/GetExternalEntity/cashApiSystemGetExternalEntityResponse.class.php
+++ lib/class/Api/System/GetExternalEntity/cashApiSystemGetExternalEntityResponse.class.php
@@ -11,7 +11,7 @@ final class cashApiSystemGetExternalEntityResponse extends cashApiAbstractRespon
             'app_name' => $externalInfoDto->getAppName(),
             'app_icon_url' => $externalInfoDto->getAppIconUrl(),
             'entity_id' => $externalInfoDto->getId(),
-            'entity_title' => $externalInfoDto->getTitle(),
+            'entity_name' => $externalInfoDto->getTitle(),
             'entity_url' => $externalInfoDto->getUrl(),
             'entity_icon' => $externalInfoDto->getIconUrl(),
             'entity_data' => $externalInfoDto->getData(),
