2731c10cb6d17a7ad630db26b2b465bd30760cf8
Konstantin Zaytsev <zaytsev.const@gmail.com>
2022-02-22 14:47:15 +0300

    chore: switch dev mode to CDN

--- client/vue.config.js
+++ client/vue.config.js
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 module.exports = {
-  publicPath: '',
+  publicPath: '/',
   productionSourceMap: false,
   filenameHashing: false,
   devServer: {
