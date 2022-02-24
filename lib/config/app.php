5de5cc56dcee086b44d66bfcddd4a06d03d5b02f
vofka <vofka@webasyst.com>
2022-02-24 12:38:32 +0300

    version 2.5.1 up

--- lib/config/app.php
+++ lib/config/app.php
@@ -2,7 +2,7 @@
 return array (
   'name' => 'Cash Flow',
   'icon' => 'img/cash.png',
-  'version' => '2.5.0',
+  'version' => '2.5.1',
   'vendor'  => '1021997',
   'rights'  => true,
   'sash_color' => '#16d15f',
