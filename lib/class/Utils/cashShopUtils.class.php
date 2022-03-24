037a09bc2e090189d2a88baad85fc6f30bf6ca71
Kirill Maramygin <kirill.maramygin@gmail.com>
2022-03-24 21:43:01 +0300

    7.1099: SS payment type list @ Cash: gray out disabled ones

--- lib/class/Utils/cashShopUtils.class.php
+++ lib/class/Utils/cashShopUtils.class.php
@@ -7,12 +7,15 @@ class cashShopUtils
         $plugins = shopPayment::getList();
 
         $model = new shopPluginModel();
-        $instances = $model->listPlugins(shopPluginModel::TYPE_PAYMENT, array('all' => true,));
+        $instances = $model->listPlugins(shopPluginModel::TYPE_PAYMENT, ['all' => true]);
         $data = [];
         foreach ($instances as &$instance) {
             $instance['installed'] = isset($plugins[$instance['plugin']]);
             if ($instance['installed']) {
-                $data[$instance['id']] = $instance['name'];
+                $data[$instance['id']] = [
+                    'enable' => (bool) $instance['status'],
+                    'name' => $instance['name'],
+                ];
             }
             unset($instance);
         }
