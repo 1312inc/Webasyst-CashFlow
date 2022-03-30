3088cea9d746a17b1f3a29acdb4044b35eac0d3e
vofka <vofka@webasyst.com>
2022-03-30 20:42:55 +0300

    C 'chart toggle' plugin version up

--- plugins/charttoggle/lib/config/plugin.php
+++ plugins/charttoggle/lib/config/plugin.php
@@ -2,7 +2,7 @@
 return [
     'name' => 'Chart Toggle',
     'img' => 'img/charttoggle.png',
-    'version' => '1.0.0',
+    'version' => '1.0.1',
     'vendor' => '1021997',
     'handlers' =>
         [
