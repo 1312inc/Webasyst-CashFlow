03fcb4c912d248b2bdded5e4a989b997fa51a6a7
Kirill Maramygin <kirill.maramygin@gmail.com>
2022-04-13 20:06:02 +0300

    7.1122: sankey chart data

--- lib/class/Report/Internal/Sankey/cashReportSankeyService.class.php
+++ lib/class/Report/Internal/Sankey/cashReportSankeyService.class.php
@@ -14,64 +14,53 @@ final class cashReportSankeyService
 
     public function getDataForYear(DateTimeImmutable $year): array
     {
-        $incomeSql = <<<SQL
-SELECT cc.name category_name, ca.name account_name, SUM(ct.amount) sankey_amount
+        $sql = <<<SQL
+(SELECT ca.currency, cc.name 'from', ca.name 'to', cc.color color, SUM(ABS(ct.amount)) value
 FROM cash_transaction ct
          JOIN cash_category cc on ct.category_id = cc.id
          JOIN cash_account ca on ca.id = ct.account_id
 WHERE ct.is_archived = 0
   AND ca.is_archived = 0
-  AND cc.type = s:category_type
+  AND cc.type = s:category_type_income
   AND ct.date >= s:date_from
   AND ct.date <= s:date_to
-GROUP BY ct.category_id, ct.account_id
-SQL;
-
-        $expenseSql = <<<SQL
-SELECT ca.name account_name, cc.name category_name, SUM(ct.amount) sankey_amount
+GROUP BY ct.category_id, ct.account_id)
+UNION ALL
+(SELECT ca.currency, ca.name 'from', cc.name 'to', '' color, SUM(ABS(ct.amount)) value
 FROM cash_transaction ct
          JOIN cash_category cc on ct.category_id = cc.id
          JOIN cash_account ca on ca.id = ct.account_id
 WHERE ct.is_archived = 0
   AND ca.is_archived = 0
-  AND cc.type = s:category_type
+  AND cc.type = s:category_type_expense
   AND ct.date >= s:date_from
   AND ct.date <= s:date_to
-GROUP BY ct.account_id, ct.category_id
+GROUP BY ct.account_id, ct.category_id)
 SQL;
 
-        $incomeData = $this->model->query($incomeSql,
+        $data = $this->model->query($sql,
             [
-                'category_type' => cashCategory::TYPE_INCOME,
+                'category_type_income' => cashCategory::TYPE_INCOME,
+                'category_type_expense' => cashCategory::TYPE_EXPENSE,
                 'date_from' => sprintf('%d-01-01', $year->format('Y')),
                 'date_to' => sprintf('%d-12-31', $year->format('Y')),
             ]
         )->fetchAll();
 
-        $expenseData = $this->model->query($expenseSql,
-            [
-                'category_type' => cashCategory::TYPE_EXPENSE,
-                'date_from' => sprintf('%d-01-01', $year->format('Y')),
-                'date_to' => sprintf('%d-12-31', $year->format('Y')),
-            ]
-        )->fetchAll();
+        return array_reduce($data, static function ($carry, $datum) {
+            $currency = $datum['currency'];
+            if (!isset($carry[$currency])) {
+                $carry[$currency] = [
+                    'details' => cashCurrencyVO::fromWaCurrency($currency),
+                    'data' => [],
+                ];
+            }
 
-        $data = [];
-        foreach ($incomeData as $incomeDatum) {
-            $data[] = [
-                'from' => $incomeDatum['category_name'],
-                'to' => $incomeDatum['account_name'],
-                'value' => (float) $incomeDatum['sankey_amount'],
-            ];
-        }
-        foreach ($expenseData as $expenseDatum) {
-            $data[] = [
-                'from' => $expenseDatum['account_name'],
-                'to' => $expenseDatum['category_name'],
-                'value' => (float) $expenseDatum['sankey_amount'],
-            ];
-        }
+            unset($datum['currency']);
+            $datum['value'] = (float) $datum['value'];
+            $carry[$currency]['data'][] = $datum;
 
-        return $data;
+            return $carry;
+        }, []);
     }
 }
