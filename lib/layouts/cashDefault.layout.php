0bcb241a5d3080fdc6490361f28d32b9e439526f
vofka <vofka@webasyst.com>
2022-04-13 15:08:19 +0300

    reviewWidget resumed

--- lib/layouts/cashDefault.layout.php
+++ lib/layouts/cashDefault.layout.php
@@ -16,7 +16,7 @@ class cashDefaultLayout extends waLayout
 
         $token = (new cashApiToken())->retrieveToken(cash()->getUser()->getContact());
         $this->executeAction('sidebar', new cashBackendSidebarAction());
-        $showReviewWidget = cash()->getModel(cashTransaction::class)->select('count(id)')->limit(30)->fetchField() == 30;
+        //$showReviewWidget = cash()->getModel(cashTransaction::class)->select('count(id)')->limit(30)->fetchField() == 30;
 
         $apiSettings = (new cashApiSystemGetSettingsHandler())->handle(null);
         $currencies = (new cashApiSystemGetCurrenciesHandler())->handle(null);
@@ -38,7 +38,7 @@ class cashDefaultLayout extends waLayout
                 'isAdmin' => (int) cash()->getUser()->canImport(),
                 'contextUser' => cash()->getUser(),
                 'userId' => (int) wa()->getUser()->getId(),
-                'show_review_widget' => $showReviewWidget,
+                //'show_review_widget' => $showReviewWidget,
                 'api_settings' => json_encode($apiSettings, JSON_UNESCAPED_SLASHES || JSON_UNESCAPED_UNICODE),
                 'currencies' => json_encode($currencies, JSON_UNESCAPED_SLASHES || JSON_UNESCAPED_UNICODE),
                 'categories' => json_encode($categories, JSON_UNESCAPED_SLASHES || JSON_UNESCAPED_UNICODE),
