16184250fa2adfd1e086f3fee2f96a670944d473
Kirill Maramygin <kirill.maramygin@gmail.com>
2022-04-18 11:45:33 +0300

    7.1122: sankey chart data

--- lib/class/Report/Internal/Sankey/cashReportSankeyHandler.class.php
+++ lib/class/Report/Internal/Sankey/cashReportSankeyHandler.class.php
@@ -13,9 +13,9 @@ final class cashReportSankeyHandler implements cashReportHandlerInterface
 
         $dateFrom = DateTimeImmutable::createFromFormat(
             'Y-m-d',
-            $params['date_from'] ?? date('Y-m-d', strtotime('-365 days'))
+            $params['from'] ?? date('Y-m-d', strtotime('-365 days'))
         );
-        $dateTo = DateTimeImmutable::createFromFormat('Y-m-d', $params['date_to'] ?? date('Y-m-d'));
+        $dateTo = DateTimeImmutable::createFromFormat('Y-m-d', $params['to'] ?? date('Y-m-d'));
         if ($dateTo === false || $dateFrom === false) {
             throw new cashValidateException('Wrong dates');
         }
@@ -29,8 +29,8 @@ final class cashReportSankeyHandler implements cashReportHandlerInterface
         return wa()->getView()->renderTemplate(
             wa()->getAppPath('templates/actions/report/internal/ReportSankey.html'),
             [
-                'dateFrom' => $dateFrom->format('Y-m-d'),
-                'dateTo' => $dateTo->format('Y-m-d'),
+                'from' => $dateFrom->format('Y-m-d'),
+                'to' => $dateTo->format('Y-m-d'),
                 'data' => json_encode($data, JSON_UNESCAPED_UNICODE),
             ],
             true
