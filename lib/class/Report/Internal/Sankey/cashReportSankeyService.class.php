720eb2df3c3ce1b36d6d36023b552fec31370cba
Kirill Maramygin <kirill.maramygin@gmail.com>
2022-04-22 09:12:33 +0300

    7.1159: Sankey: accounts are displayed on the left (with 'income' categories) sometimes

--- lib/class/Report/Internal/Sankey/cashReportSankeyService.class.php
+++ lib/class/Report/Internal/Sankey/cashReportSankeyService.class.php
@@ -15,7 +15,7 @@ final class cashReportSankeyService
     public function getDataForPeriod(DateTimeImmutable $dateFrom, DateTimeImmutable $dateTo): array
     {
         $sql = <<<SQL
-(SELECT ca.currency, cc.name 'from', ca.name 'to', cc.color color, SUM(ABS(ct.amount)) value
+(SELECT ca.currency, cc.name 'from', ca.name 'to', cc.color color, SUM(ABS(ct.amount)) value, 'income' direction
 FROM cash_transaction ct
          JOIN cash_category cc on ct.category_id = cc.id
          JOIN cash_account ca on ca.id = ct.account_id
@@ -26,7 +26,7 @@ WHERE ct.is_archived = 0
   AND ct.date <= s:date_to
 GROUP BY ct.category_id, ct.account_id)
 UNION ALL
-(SELECT ca.currency, ca.name 'from', cc.name 'to', cc.color color, SUM(ABS(ct.amount)) value
+(SELECT ca.currency, ca.name 'from', cc.name 'to', cc.color color, SUM(ABS(ct.amount)) value, 'expense' direction
 FROM cash_transaction ct
          JOIN cash_category cc on ct.category_id = cc.id
          JOIN cash_account ca on ca.id = ct.account_id
@@ -38,7 +38,8 @@ WHERE ct.is_archived = 0
 GROUP BY ct.account_id, ct.category_id)
 SQL;
 
-        $data = $this->model->query($sql,
+        $data = $this->model->query(
+            $sql,
             [
                 'category_type_income' => cashCategory::TYPE_INCOME,
                 'category_type_expense' => cashCategory::TYPE_EXPENSE,
@@ -47,7 +48,7 @@ SQL;
             ]
         )->fetchAll();
 
-        return array_reduce($data, static function ($carry, $datum) {
+        $chartData = array_reduce($data, static function ($carry, $datum) {
             $currency = $datum['currency'];
             if (!isset($carry[$currency])) {
                 $carry[$currency] = [
@@ -62,5 +63,36 @@ SQL;
 
             return $carry;
         }, []);
+
+        foreach ($chartData as $currency => $datum) {
+            array_walk($chartData[$currency]['data'], static function (&$item) use ($datum) {
+                $item['currency'] = $datum['details']->getCode();
+                $item['currencySign'] = $datum['details']->getSign();
+            });
+
+            $income = array_filter($datum['data'], static function (array $line) {
+                return $line['direction'] === 'income';
+            });
+            $expense = array_filter($datum['data'], static function (array $line) {
+                return $line['direction'] === 'expense';
+            });
+
+            $incomeUnique = array_unique(array_column($income, 'to'));
+            foreach ($expense as $item) {
+                if (!in_array($item['from'], $incomeUnique, true)) {
+                    $chartData[$currency]['data'][] = [
+                        'from' => 'stub',
+                        'to' => $item['from'],
+                        'value' => 0,
+                        'direction' => 'income',
+                        'color' => '',
+                        'currency' => $datum['details']->getCode(),
+                        'currencySign' => $datum['details']->getSign(),
+                    ];
+                }
+            }
+        }
+
+        return $chartData;
     }
 }
