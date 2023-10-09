a3e375424fcba52dd6cd9d2a5194e2f234fa621c
Konstantin Zaytsev <zaytsev.const@gmail.com>
2023-10-09 13:39:31 +0300

    SSR Reports > Sankey > add 'Next ...' options #106.9

--- js/reports/rangeToggle.js
+++ js/reports/rangeToggle.js
@@ -9,29 +9,38 @@ function isValidDate (string) {
 
 export default function (selector, rangesDays, callback) {
 
-    const from = $(`${selector} [name="from"]`).val();
-    const to = $(`${selector} [name="to"]`).val();
+    const $dropdownToggle = $(`${selector} .dropdown-toggle`);
 
     $(`${selector} #range-dropdown`).waDropdown({
         items: ".menu > li > a",
+        hide: false,
         ready: function () {
+            let from = $(`${selector} [name="from"]`).val();
+            let to = $(`${selector} [name="to"]`).val();
+            const nextMode = from === new Date().toISOString().slice(0, 10);
+
+            if (nextMode) {
+                [from, to] = [to, from];
+            }
+
             if (
                 isValidDate(from) &&
                 isValidDate(to)
             ) {
                 for (const days of rangesDays) {
+                    const targetButton = $(`${selector} [data-range="${days}"]`)[0];
                     if (
                         to === new Date().toISOString().slice(0, 10) &&
                         from === new Date(new Date().setDate(new Date().getDate() - days)).toISOString().slice(0, 10) &&
-                        $(`${selector} [data-range="${days}"]`)[0]
+                        targetButton
                     ) {
-                        $(`${selector} [data-range="${days}"]`).trigger('click');
+                        $dropdownToggle.text(targetButton.innerText);
                         return;
                     }
                 }
                 $(`${selector} .dateFrom`).val(toLocaleDate(from));
                 $(`${selector} .dateTo`).val(toLocaleDate(to));
-                $(`${selector} [data-range="custom"]`).trigger('click');
+                $(`${selector} .range-manual`).show();
             }
         },
         change: function (event, target, dropdown) {
@@ -41,8 +50,9 @@ export default function (selector, rangesDays, callback) {
                 $(`${selector} .range-manual`).hide();
                 const from = new Date(new Date().setDate(new Date().getDate() - days)).toISOString().slice(0, 10);
                 const to = new Date().toISOString().slice(0, 10);
-                $(`${selector} [name="from"]`).val(from);
-                $(`${selector} [name="to"]`).val(to);
+                const nextMode = Date.parse(from) - Date.parse(to) > 0;
+                $(`${selector} [name="from"]`).val(nextMode ? to : from);
+                $(`${selector} [name="to"]`).val(nextMode ? from : to);
 
                 if (oldValue !== from) {
                     if (typeof callback === 'function') {
