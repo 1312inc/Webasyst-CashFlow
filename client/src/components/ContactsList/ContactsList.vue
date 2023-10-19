ac91aee0bbf99080f8ccdbef1d0f2239ca0cbde9
Konstantin Zaytsev <zaytsev.const@gmail.com>
2023-10-19 14:38:47 +0300

    sidebar > 'Contacts' toggle > zero reponse > add .hint disclaimer #106.17

--- client/src/components/ContactsList/ContactsList.vue
+++ client/src/components/ContactsList/ContactsList.vue
@@ -1,28 +1,26 @@
 <template>
   <div>
-    <ul
-      v-if="list.length"
-      class="menu"
-    >
-      <li
-        v-for="contact in list"
-        :key="contact.id"
-        :class="{
-          selected:
-            +$route.params.id === contact.id && $route.name === 'Contact'
-        }"
-      >
-        <router-link :to="`/contact/${contact.id}`">
-          <div class="wide">
-            <div class="flexbox">
-              <span class="icon"><img
-                :src="contact.photo_url_absolute"
-                alt=""
-                class="userpic"
-              ></span>
-              <div class="wide">
-                <span>{{ contact.name }}</span>
-              </div>
+    <template v-if="list.length">
+      <ul class="menu">
+        <li
+          v-for="contact in list"
+          :key="contact.id"
+          :class="{
+            selected:
+              +$route.params.id === contact.id && $route.name === 'Contact'
+          }"
+        >
+          <router-link :to="`/contact/${contact.id}`">
+            <div class="wide">
+              <div class="flexbox">
+                <span class="icon"><img
+                  :src="contact.photo_url_absolute"
+                  alt=""
+                  class="userpic"
+                ></span>
+                <div class="wide">
+                  <span>{{ contact.name }}</span>
+                </div>
               <!-- <span class="count">
                 <span
                   v-for="(currency, i) in contact.stat"
@@ -31,43 +29,49 @@
                   {{ currency.data.summaryShorten }} {{ $helper.currencySignByCode(currency.currency) }}<span v-if="i < contact.stat.length - 1">,</span>
                 </span>
               </span> -->
-            </div>
-            <div class="flexbox">
-              <span class="icon" />
-              <div class="smaller hint custom-mt-4">
-                {{ contact.last_transaction.amountShorten }}
-                {{ $helper.currencySignByCode(contact.last_transaction.currency) }},
-                {{
-                  $moment(contact.last_transaction.date).format("D MMMM")
-                }}
+              </div>
+              <div class="flexbox">
+                <span class="icon" />
+                <div class="smaller hint custom-mt-4">
+                  {{ contact.last_transaction.amountShorten }}
+                  {{ $helper.currencySignByCode(contact.last_transaction.currency) }},
+                  {{
+                    $moment(contact.last_transaction.date).format("D MMMM")
+                  }}
+                </div>
               </div>
             </div>
-          </div>
-        </router-link>
-      </li>
-      <template v-if="loading">
-        <li
-          v-for="i in limit"
-          :key="i"
-          class="skeleton custom-px-16"
-          style="padding: 6px 0;"
-        >
-          <span
-            class="skeleton-list"
-            style="margin-bottom: 0;"
-          />
+          </router-link>
         </li>
-      </template>
-    </ul>
+        <template v-if="loading">
+          <li
+            v-for="i in limit"
+            :key="i"
+            class="skeleton custom-px-16"
+            style="padding: 6px 0;"
+          >
+            <span
+              class="skeleton-list"
+              style="margin-bottom: 0;"
+            />
+          </li>
+        </template>
+      </ul>
+      <div
+        v-if="list.length < total"
+        class="custom-mx-16 align-center"
+      >
+        <a
+          class="button light-gray rounded smaller"
+          @click.prevent="offset += limit"
+        >{{ $t("showMore") }}</a>
+      </div>
+    </template>
     <div
-      v-if="list.length < total"
-      class="custom-mx-16 align-center"
+      v-else
+      class="hint align-center custom-my-12"
     >
-      <a
-        href="#"
-        class="button light-gray rounded smaller"
-        @click.prevent="offset += limit"
-      >{{ $t("showMore") }}</a>
+      {{ $t('contactsWillSoon') }}
     </div>
   </div>
 </template>
