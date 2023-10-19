2a0ddacc15868173b5bba2732f276146839460f7
Konstantin Zaytsev <zaytsev.const@gmail.com>
2023-10-19 12:56:04 +0300

    tune layout style #106.12

--- client/src/components/Sidebar/SidebarFooter.vue
+++ client/src/components/Sidebar/SidebarFooter.vue
@@ -2,26 +2,26 @@
   <ul class="menu">
     <li v-if="$permissions.canSeeReport">
       <a :href="`${$helper.baseUrl}report/dds/`">
-        <i class="fas fa-chart-pie"></i>
+        <i class="fas fa-chart-pie" />
         <span>{{ $t("reports") }}</span>
       </a>
     </li>
     <li v-if="$permissions.canImport">
       <a :href="`${$helper.baseUrl}import/`">
-        <i class="fas fa-file-import"></i>
+        <i class="fas fa-file-import" />
         <span>{{ $t("import") }}</span>
       </a>
     </li>
     <li v-if="$permissions.isAdmin">
       <router-link to="/trash">
-        <i class="fas fa-trash-alt"></i>
+        <i class="fas fa-trash-alt" />
         <span>{{ $t("trash") }}</span>
       </router-link>
     </li>
     <li v-if="$permissions.isAdmin">
       <a :href="`${$helper.baseUrl}plugins/`">
-        <i class="fas fa-plug"></i>
-        <span>Plugins</span>
+        <i class="fas fa-plug" />
+        <span>{{ $t("plugins") }}</span>
       </a>
     </li>
   </ul>
