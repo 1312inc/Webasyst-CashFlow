87a6b13ddd3cfb99ca60b64cd2f63f110f88fb4c
Konstantin Zaytsev <zaytsev.const@gmail.com>
2023-10-23 13:55:00 +0300

    SS Import UI > after-import welcome notice #106.26

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,14 @@
 <template>
-  <div class="content hide-scrollbar blank">
+  <DialogProvider class="content hide-scrollbar blank">
     <div
       sticky-container
       class="box custom-px-8 custom-p-0-mobile"
     >
       <router-view />
     </div>
-  </div>
+  </DialogProvider>
 </template>
+
+<script setup>
+import DialogProvider from './components/DialogProvider.vue'
+</script>
