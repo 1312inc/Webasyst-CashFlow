9325dc03c01d15be7fd2ad56cdc3e1648209cbc4
Vladimir V. Tuporshin <vofka@vofka.ru>
2023-10-27 18:22:05 +0300

    2.8.0 version up!

--- lib/config/app.php
+++ lib/config/app.php
@@ -2,7 +2,7 @@
 return array (
   'name' => 'Cash Flow',
   'icon' => 'img/cash.png',
-  'version' => '2.7.3',
+  'version' => '2.8.0',
   'vendor'  => '1021997',
   'rights'  => true,
   'sash_color' => '#16d15f',
