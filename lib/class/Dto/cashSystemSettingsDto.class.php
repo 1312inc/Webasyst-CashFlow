96b71cf42f4ea34a4278239eeccb0bc02b4992f1
Leonix <leonix@motd.ru>
2023-11-07 11:30:29 +0300

    106.76: cash.system.getSettings > 'isShop' boolean var

--- lib/class/Dto/cashSystemSettingsDto.class.php
+++ lib/class/Dto/cashSystemSettingsDto.class.php
@@ -6,12 +6,15 @@ class cashSystemSettingsDto
 
     public $userId;
 
+    public $isShop;
+
     public $rights;
 
     public function __construct($userId, cashContactRightsDto $rights)
     {
         $this->ts = time();
         $this->userId = $userId;
+        $this->isShop = (int)wa()->appExists('shop');
         $this->rights = $rights;
     }
 }
