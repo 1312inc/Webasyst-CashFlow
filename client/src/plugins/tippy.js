79997736d199829a76e9240ebe24af8ff9baf5ac
Konstantin Zaytsev <zaytsev.const@gmail.com>
2023-11-20 17:10:01 +0300

    Update tippy.js #106.111

--- client/src/plugins/tippy.js
+++ client/src/plugins/tippy.js
@@ -3,15 +3,16 @@ export default {
     Vue.directive('wa-tippy', {
       inserted (el, binding, vnode) {
         // define global tippy object from the WA
-        const tippy = window.tippy || function () { }
-        Vue.nextTick(() => {
-          if (binding.value) {
-            tippy(el, {
-              content: binding.value,
-              arrow: false
-            })
-          }
-        })
+        window.tippy &&
+          Vue.nextTick(() => {
+            if (binding.value) {
+              window.tippy(el, {
+                content: binding.value,
+                arrow: false,
+                zIndex: 99999
+              })
+            }
+          })
       },
       unbind (el) {
         el._tippy && el._tippy.destroy()
