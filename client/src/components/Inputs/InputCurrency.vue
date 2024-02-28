be2a8393a1fea2d17ce0746a604df2de6e08e4c7
Konstantin Zaytsev <zaytsev.const@gmail.com>
2024-02-28 15:47:45 +0300

    Update InputCurrency.vue

--- client/src/components/Inputs/InputCurrency.vue
+++ client/src/components/Inputs/InputCurrency.vue
@@ -17,6 +17,7 @@
         class="number bold width-100"
         :style="category && `border-color: ${category.color}`"
         type="text"
+        inputmode="numeric"
         @accept="$emit('input', $event)"
         @keyup.enter="$emit('keyEnter', $event)"
       />
