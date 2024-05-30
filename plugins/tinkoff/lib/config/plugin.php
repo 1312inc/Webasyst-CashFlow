84263f5e60a21b4ac5626948d14a1d10238b90df
Vladimir V. Tuporshin <vofka@vofka.ru>
2024-05-30 05:52:31 +0300

    T plugin version up 1.0.0

--- plugins/tinkoff/lib/config/plugin.php
+++ plugins/tinkoff/lib/config/plugin.php
@@ -6,9 +6,9 @@ if (!defined('TINKOFF_FILE_LOG')) {
 
 return array(
     'name' => 'Тинькофф Бизнес',
-    'description' => 'Импорт операций из «Тинькофф Бизнес» по API',
+    'description' => 'Импорт операций из «Тинькофф Бизнес»',
     'img' => 'img/tinkoff.svg',
-    'version' => '0.0.1',
+    'version' => '1.0.0',
     'vendor' => 'webasyst',
     'import_api' => true,
     'handlers' => [
