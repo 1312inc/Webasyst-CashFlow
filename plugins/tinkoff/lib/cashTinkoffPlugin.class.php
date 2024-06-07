f387e746ae27789f58444a52c7142c31d0ab5f88
moroz <ivan.moroz@webasyst.ru>
2024-06-07 20:08:22 +0300

    #106.381: plugin tinkoff > auto update T import via a mobile app

--- plugins/tinkoff/lib/cashTinkoffPlugin.class.php
+++ plugins/tinkoff/lib/cashTinkoffPlugin.class.php
@@ -446,7 +446,6 @@ class cashTinkoffPlugin extends cashBusinessPlugin
     }
 
     /**
-     * Event on_count
      * Event api_transaction.get_list
      *
      * @param $event
@@ -454,9 +453,6 @@ class cashTinkoffPlugin extends cashBusinessPlugin
      */
     public function cashEventOnCountTinkoffHandler($event)
     {
-        if ($event->getName() !== cashEventStorage::ON_COUNT && !waRequest::isMobile()) {
-            return;
-        }
         $profiles = self::getProfiles();
         if (empty($profiles)) {
             return;
