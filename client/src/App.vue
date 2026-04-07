44fa7bda03d89d99090d566948f3eb46e78f081a
Konstantin Zaytsev <zaytsev.const@gmail.com>
2026-04-07 16:05:12 +0300

    onboarding > deprecate SS 'welcome' dialog #106.447

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,14 @@
 <template>
-  <DialogProvider class="content custom-pb-24 custom-ml-12 custom-mr-24 custom-mr-12-mobile">
+  <div class="content custom-pb-24 custom-ml-12 custom-mr-24 custom-mr-12-mobile">
     <router-view />
     <TransactionModals />
-  </DialogProvider>
+  </div>
 </template>
 
 <script>
-import DialogProvider from './components/DialogProvider.vue'
 
 export default {
   components: {
-    DialogProvider,
     TransactionModals: () => import('./components/TransactionModals.vue')
   }
 }
