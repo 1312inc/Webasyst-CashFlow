ab36ee7b438b6dabb47912d32822adbdfba07586
Konstantin Zaytsev <zaytsev.const@gmail.com>
2026-04-23 15:37:17 +0300

    add Sentry for the dev mode

--- client/src/desktop.js
+++ client/src/desktop.js
@@ -19,6 +19,8 @@ import eventBus from './plugins/eventBus'
 import { Plugin } from 'vue-fragment'
 import './assets/styles/styles.css'
 
+import * as Sentry from '@sentry/vue'
+
 Vue.config.productionTip = false
 Vue.prototype.$isSpaMobileMode = false
 
@@ -39,6 +41,16 @@ const contentSelector = '#app-content'
 const sidebarSelector = '#app-sidebar'
 
 if (document.querySelector(contentSelector)) {
+  if (import.meta.env.MODE === 'development') {
+    Sentry.init({
+      Vue,
+      dsn: 'https://83304f9c91de37513866d2f606b18021@o183199.ingest.us.sentry.io/4511269338546176',
+      // Setting this option to true will send default PII data to Sentry.
+      // For example, automatic IP address collection on events
+      sendDefaultPii: true
+    })
+  }
+
   new Vue({
     router,
     store,
