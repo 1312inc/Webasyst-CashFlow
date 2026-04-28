dc6b7b84a6c47c1fe96e8bb6c675cd676c647ed3
moroz <ivan.moroz@webasyst.ru>
2026-04-28 21:29:24 +0300

    #106.493: plan > cash.plan.get > month = null

--- lib/class/Api/Plan/Get/cashApiPlanGetHandler.class.php
+++ lib/class/Api/Plan/Get/cashApiPlanGetHandler.class.php
@@ -14,7 +14,26 @@ class cashApiPlanGetHandler implements cashApiHandlerInterface
     public function handle($request)
     {
         $model = cash()->getModel('cashPlan');
-        $where = ['(cp.`month` IS NULL OR cp.`month` >= s:date_from AND cp.`month` < DATE_ADD(s:date_to, INTERVAL 1 DAY))'];
+
+        if (empty($request->date) || is_numeric($request->date)) {
+            if (empty($request->date)) {
+                /** for null */
+                $where = ['cp.`month` IS NULL'];
+            } else {
+                /** for year */
+                $where = ['(cp.`month` IS NULL OR cp.`month` >= s:date_from AND cp.`month` < DATE_ADD(s:date_to, INTERVAL 1 DAY))'];
+            }
+            $year = (empty($request->date) ? date('Y') : $request->date);
+            $date_from = $year.'-01-01';
+            $date_to = $year.'-12-31';
+        } else {
+            /** for month */
+            $date = DateTimeImmutable::createFromFormat('Y-m-d|', $request->date);
+            $date_from = $date->modify('first day of this month')->format('Y-m-d');
+            $date_to = $date->modify('last day of this month')->format('Y-m-d');
+            $where = ['(cp.`month` IS NULL OR cp.`month` >= s:date_from AND cp.`month` < DATE_ADD(s:date_to, INTERVAL 1 DAY))'];
+        }
+
         if ($request->id) {
             $where = ['cp.id = i:plan_id'];
         } else {
@@ -28,7 +47,6 @@ class cashApiPlanGetHandler implements cashApiHandlerInterface
                 $where[] = '(cp.account_id = i:account_id OR cp.account_id IS NULL)';
             }
         }
-        $date = DateTimeImmutable::createFromFormat('Y-m-d|', $request->date);
 
         return $model->query("
             SELECT cp.id, cp.currency, cp.account_id, cp.category_id, s:date_from `from`, s:date_to `to`, cp.amount, SUM(IF(ct.amount, ct.amount, 0)) amount_fact
@@ -39,8 +57,8 @@ class cashApiPlanGetHandler implements cashApiHandlerInterface
             GROUP BY cp.id, cp.currency, cp.account_id, cp.category_id, cp.amount 
             ORDER BY cp.currency, cp.account_id, cp.category_id
         ", [
-            'date_from'   => $date->modify('first day of this month')->format('Y-m-d'),
-            'date_to'     => $date->modify('last day of this month')->format('Y-m-d'),
+            'date_from'   => $date_from,
+            'date_to'     => $date_to,
             'currency'    => $request->currency,
             'category_id' => $request->category_id,
             'account_id'  => $request->account_id,
