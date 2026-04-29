176ed1ef9837f1f4128ce205f29515af0853fa73
moroz <ivan.moroz@webasyst.ru>
2026-04-29 14:48:29 +0300

    #106.493: plan > cash.plan.get > month = null

--- lib/class/Api/Plan/Get/cashApiPlanGetHandler.class.php
+++ lib/class/Api/Plan/Get/cashApiPlanGetHandler.class.php
@@ -13,21 +13,18 @@ class cashApiPlanGetHandler implements cashApiHandlerInterface
      */
     public function handle($request)
     {
+        $join = [];
         $model = cash()->getModel('cashPlan');
-
-        if (empty($request->date) || is_numeric($request->date)) {
-            if (empty($request->date)) {
-                /** for null */
-                $where = ['cp.`month` IS NULL'];
-            } else {
-                /** for year */
-                $where = ['(cp.`month` IS NULL OR cp.`month` >= s:date_from AND cp.`month` < DATE_ADD(s:date_to, INTERVAL 1 DAY))'];
-            }
-            $year = (empty($request->date) ? date('Y') : $request->date);
-            $date_from = $year.'-01-01';
-            $date_to = $year.'-12-31';
+        $select = ['cp.id', 'cp.currency', 'cp.account_id', 'cp.category_id'];
+        if (empty($request->date)) {
+            $select = array_merge($select, ['NULL `from`, NULL `to`, cp.amount, NULL amount_fact']);
+            $where = ['cp.`month` IS NULL'];
         } else {
-            /** for month */
+            $select = array_merge($select, ['s:date_from `from`', 's:date_to `to`', 'cp.amount', 'SUM(IF(ct.amount, ct.amount, 0)) amount_fact']);
+            $join = [
+                'LEFT JOIN cash_transaction ct ON ct.category_id = cp.category_id AND ct.`date` >= s:date_from AND ct.`date` < DATE_ADD(s:date_to, INTERVAL 1 DAY)',
+                'LEFT JOIN cash_account ca ON ca.id = ct.account_id AND ca.currency = cp.currency'
+            ];
             $date = DateTimeImmutable::createFromFormat('Y-m-d|', $request->date);
             $date_from = $date->modify('first day of this month')->format('Y-m-d');
             $date_to = $date->modify('last day of this month')->format('Y-m-d');
@@ -49,16 +46,15 @@ class cashApiPlanGetHandler implements cashApiHandlerInterface
         }
 
         return $model->query("
-            SELECT cp.id, cp.currency, cp.account_id, cp.category_id, s:date_from `from`, s:date_to `to`, cp.amount, SUM(IF(ct.amount, ct.amount, 0)) amount_fact
+            SELECT ".implode(',', $select)."
             FROM cash_plan cp 
-            LEFT JOIN cash_transaction ct ON ct.category_id = cp.category_id AND ct.`date` >= s:date_from AND ct.`date` < DATE_ADD(s:date_to, INTERVAL 1 DAY)
-            LEFT JOIN cash_account ca ON ca.id = ct.account_id AND ca.currency = cp.currency 
+            ".implode('', $join)."
             WHERE ".implode(' AND ', $where)."
             GROUP BY cp.id, cp.currency, cp.account_id, cp.category_id, cp.amount 
             ORDER BY cp.currency, cp.account_id, cp.category_id
         ", [
-            'date_from'   => $date_from,
-            'date_to'     => $date_to,
+            'date_from'   => ifset($date_from, ''),
+            'date_to'     => ifset($date_to, ''),
             'currency'    => $request->currency,
             'category_id' => $request->category_id,
             'account_id'  => $request->account_id,
