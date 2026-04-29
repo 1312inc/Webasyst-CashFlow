a4ac6ef011c6e721b454c9d9a0518ce1c38f89f8
Vladimir V. Tuporshin <vofka@vofka.ru>
2026-04-29 13:49:30 +0300

    106.452:Dev: 'cash_automation' > bring the link back (wip)

--- client/src/components/Sidebar/SidebarFooter.vue
+++ client/src/components/Sidebar/SidebarFooter.vue
@@ -16,7 +16,7 @@ import { appState } from '@/utils/appState'
         <span>{{ $t("plugins") }}</span>
       </a>
     </li>
-    <li v-if="$permissions.isAdmin" style="display: none;">
+    <li v-if="$permissions.isAdmin">
       <a :href="`${$helper.baseUrl}automation/`">
         <i class="fas fa-plug" />
         <span>{{ $t("automation") }}</span>
