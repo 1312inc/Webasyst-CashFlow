0c94fd3fd8310f23654c177d46b0ebfd234b5361
Konstantin Zaytsev <zaytsev.const@gmail.com>
2026-05-04 14:54:39 +0300

    Update api.js

--- client/src/plugins/api.js
+++ client/src/plugins/api.js
@@ -15,48 +15,48 @@ const api = axios.create({
   }
 })
 
-// api.interceptors.response.use((response) => {
-//   if (!response.headers['content-type'].includes('application/json')) {
-//     store.commit('errors/error', {
-//       title: 'error.api',
-//       method: i18n.t('error.nonJsonTitle'),
-//       message: i18n.t('error.nonJsonText')
-//     })
-//   }
-//   // if (response.data?.error) {
-//   //   store.commit('errors/error', {
-//   //     title: 'error.api',
-//   //     method: response.config.url,
-//   //     message: response.data.error_description
-//   //   })
-//   // }
-//   return response
-// }, (error) => {
-//   if (!error.response.headers['content-type'].includes('application/json')) {
-//     store.commit('errors/error', {
-//       title: 'error.api',
-//       method: error.response.config.url,
-//       message: i18n.t('error.nonJsonTitle')
-//     })
-//     return Promise.reject(error)
-//   }
-//   if (error.response.data?.error) {
-//     // if (error.response.status !== 404) {
-//     //   store.commit('errors/error', {
-//     //     title: 'error.api',
-//     //     method: error.response.config.url,
-//     //     message: error.response.data.error_description
-//     //   })
-//     // }
-//   } else {
-//     store.commit('errors/error', {
-//       title: 'error.http',
-//       method: error.response.config.url,
-//       message: error.response.data
-//     })
-//   }
-//   return Promise.reject(error)
-// })
+api.interceptors.response.use((response) => {
+  if (!response.headers['content-type'].includes('application/json')) {
+    store.commit('errors/error', {
+      title: 'error.api',
+      method: i18n.t('error.nonJsonTitle'),
+      message: i18n.t('error.nonJsonText')
+    })
+  }
+  // if (response.data?.error) {
+  //   store.commit('errors/error', {
+  //     title: 'error.api',
+  //     method: response.config.url,
+  //     message: response.data.error_description
+  //   })
+  // }
+  return response
+}, (error) => {
+  if (!error.response.headers['content-type'].includes('application/json')) {
+    store.commit('errors/error', {
+      title: 'error.api',
+      method: error.response.config.url,
+      message: i18n.t('error.nonJsonTitle')
+    })
+    return Promise.reject(error)
+  }
+  if (error.response.data?.error) {
+    // if (error.response.status !== 404) {
+    //   store.commit('errors/error', {
+    //     title: 'error.api',
+    //     method: error.response.config.url,
+    //     message: error.response.data.error_description
+    //   })
+    // }
+  } else {
+    store.commit('errors/error', {
+      title: 'error.http',
+      method: error.response.config.url,
+      message: error.response.data
+    })
+  }
+  return Promise.reject(error)
+})
 
 export { baseApiUrl, accessToken }
 
