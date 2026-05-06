ca868915308117a53b3e405f92efefa00d27941e
Konstantin Zaytsev <zaytsev.const@gmail.com>
2026-05-06 15:30:16 +0300

    /budget won't click from SSR #106.517

--- client/src/components/Bricks/Bricks.vue
+++ client/src/components/Bricks/Bricks.vue
@@ -3,19 +3,18 @@
   <div class="bricks custom-mt-0">
     <BrickTransactions />
     <BrickCalendar />
-    <a
-      :href="`${$helper.baseUrl}plan/`"
+    <router-link
+      to="/budget"
       class="brick custom-pt-8"
       :class="{
         'selected': $route.name === 'Plan'
       }"
-      @click.prevent="() => { $router.push('/budget') }"
     >
       <div class="flexbox middle space-8">
         <span class="icon"><i class="fas fa-tachometer-alt text-green" /></span>
         <span>{{ $t("plan") }}</span>
       </div>
-    </a>
+    </router-link>
     <a
       v-if="$permissions.canSeeReport"
       :href="`${$helper.baseUrl}report/dds/`"
