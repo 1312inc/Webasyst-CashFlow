777a1c27c2686a2cc31b8f95f441530066236be3
moroz <ivan.moroz@webasyst.ru>
2026-05-08 09:55:01 +0300

    #106.493: plan > cash.plan.get общий план

--- lib/class/Api/Plan/Get/cashApiPlanGetHandler.class.php
+++ lib/class/Api/Plan/Get/cashApiPlanGetHandler.class.php
@@ -26,21 +26,35 @@ class cashApiPlanGetHandler implements cashApiHandlerInterface
             $where[] = 'cp.category_id = i:category_id';
         }
         if (isset($request->account_id)) {
-            $where[] = '(cp.account_id = i:account_id OR cp.account_id IS NULL)';
+            $where[] = '(cp.account_id = i:account_id)';
         }
-        if (!empty($request->date)) {
-            $date = DateTimeImmutable::createFromFormat('Y-m-d|', $request->date);
-            $date_from = $date->modify('first day of this month')->format('Y-m-d');
-            $date_to = $date->modify('last day of this month')->format('Y-m-d');
-            $where[] = '(cp.`month` IS NULL OR (cp.`month` >= @date_from AND cp.`month` < DATE_ADD(@date_to, INTERVAL 1 DAY)))';
+        if (empty($request->date)) {
+            $where[] = 'cp.`month` IS NULL';
+            return $model->query("
+                SELECT cp.id, cp.currency, cp.account_id, cp.category_id, NULL `from`, NULL `to`, cp.amount, NULL amount_fact             
+                FROM cash_plan cp
+                LEFT JOIN cash_account ca ON ca.currency = cp.currency
+                WHERE ".implode(' AND ', $where)."
+                GROUP BY cp.id, cp.currency, cp.account_id, cp.category_id, cp.amount 
+                ORDER BY cp.currency, cp.account_id, cp.category_id
+            ", [
+                'currency'    => $request->currency,
+                'category_id' => $request->category_id,
+                'account_id'  => $request->account_id,
+            ])->fetchAll();
         }
+
+        $date = DateTimeImmutable::createFromFormat('Y-m-d|', $request->date);
+        $date_from = $date->modify('first day of this month')->format('Y-m-d');
+        $date_to = $date->modify('last day of this month')->format('Y-m-d');
+        $where[] = '(cp.`month` IS NULL OR (cp.`month` >= @date_from AND cp.`month` < DATE_ADD(@date_to, INTERVAL 1 DAY)))';
 
         $model->exec('SET @date_from:= '.($date_from ? "'$date_from'" : 'NULL'));
         $model->exec('SET @date_to:= '.($date_to ? "'$date_to'" : 'NULL'));
 
         return $model->query("
-            SELECT cp.id, cp.currency, cp.account_id, cp.category_id, IF(cp.`month` IS NULL, cp.`month`, @date_from) `from`, IF(cp.`month` IS NULL, cp.`month`, @date_to) `to`, cp.amount, SUM(IF(ct.amount, ct.amount, 0)) amount_fact
-            FROM cash_plan cp 
+            SELECT cp.id, cp.currency, cp.account_id, cp.category_id, IF(cp.`month` IS NULL, cp.`month`, @date_from) `from`, IF(cp.`month` IS NULL, cp.`month`, @date_to) `to`, cp.amount, IF(@date_to IS NULL, NULL, SUM(IF(ct.amount, ct.amount, 0))) amount_fact             
+            FROM cash_plan cp
             LEFT JOIN cash_account ca ON ca.currency = cp.currency
             LEFT JOIN cash_transaction ct ON ca.id = ct.account_id AND ct.category_id = cp.category_id 
             AND CASE
