e4de8d13410033a7ce7fc95772326342c3188475
moroz <ivan.moroz@webasyst.ru>
2026-05-12 11:50:43 +0300

    #106.452: 'cash_automation' > action, plugin

--- plugins/testautomation/lib/cashTestautomationPlugin.class.php
+++ plugins/testautomation/lib/cashTestautomationPlugin.class.php
@@ -2,24 +2,53 @@
 
 class cashTestautomationPlugin extends waPlugin
 {
-    public function cashEventViewTestautomationHandler()
+    private static function getConditions()
     {
         return [
-            'conditions' => [
-                'by_user' => ['name' => 'Имя пользователя', 'operators' => ['==', '<>', '??']],
-                'by_inn'  => ['name' => 'ИНН', 'operators' => ['^...$', '==', '!=']],
-                'by_bank' => ['name' => 'Банк', 'operators' => ['#...#', '==', '!=']],
-            ],
-            'actions' => [
-                'repeat_transaction' => 'Создавать рекурентную операцию',
-                'send_sms'           => 'Отправить СМС',
-                'create_reminder'    => 'Создать напоминание',
-            ]
+            'testautomation_by_user' => ['name' => 'Имя пользователя', 'operators' => ['==', '<>', '??']],
+            'testautomation_by_inn'  => ['name' => 'ИНН', 'operators' => ['^...$', '==', '!=']],
+            'testautomation_by_bank' => ['name' => 'Банк', 'operators' => ['#...#', '==', '!=']],
         ];
     }
 
-    public function cashEventTestautomationHandler()
+    private static function getActions()
     {
-        return [];
+        return  [
+            'testautomation_repeat_transaction' => 'Создавать рекурентную операцию',
+            'testautomation_send_sms'           => 'Отправить СМС',
+            'testautomation_create_reminder'    => 'Создать напоминание',
+        ];
+    }
+
+    public function cashEventViewTestautomationHandler()
+    {
+        return [
+            'conditions' => self::getConditions(),
+            'actions' => self::getActions()
+        ];
+    }
+
+    /**
+     * @param $params
+     * @return bool
+     */
+    public function cashEventTestautomationHandler($params = [])
+    {
+        $conditions = self::getConditions();
+        $condition = ifset($params, 'condition', 'condition_id', '');
+        if (empty($conditions[$condition])) {
+            cash()->getLogger()->log(['В плагине нет такого условия для выполнения', 'PARAMS' => $params], cashHelper::AUTOMATION_LOG);
+            return false;
+        }
+        $actions = self::getActions();
+        $action = ifset($params, 'action', '');
+        if (empty($actions[$action])) {
+            cash()->getLogger()->log(['В плагине нет такого действия для выполнения', 'PARAMS' => $params], cashHelper::AUTOMATION_LOG);
+            return false;
+        }
+
+        cash()->getLogger()->log(['Плагин выполнил: '.$actions[$action].' C условием: '.var_export($conditions[$condition], true), 'PARAMS' => $params], cashHelper::AUTOMATION_LOG);
+
+        return true;
     }
 }
