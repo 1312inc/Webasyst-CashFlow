4ad7270377d1dabc96f583e7216ad4e31bea802c
moroz <ivan.moroz@webasyst.ru>
2026-05-13 14:23:20 +0300

    #106.512: Dev: /report/ stream > exclude all 'is_imaginary == -1'

--- lib/class/Report/Internal/Stream/cashReportStreamService.class.php
+++ lib/class/Report/Internal/Stream/cashReportStreamService.class.php
@@ -16,62 +16,58 @@ final class cashReportStreamService
     {
         $grouping = new cashReportSankeyDataGrouping($dateFrom, $dateTo);
 
-        $sqlCurrencies = <<<SQL
-SELECT ca.currency
-FROM cash_transaction ct
-         JOIN cash_account ca on ca.id = ct.account_id
-WHERE ct.is_archived = 0
-  AND ca.is_archived = 0
-  AND ct.date >= s:date_from
-  AND ct.date <= s:date_to
-  AND ct.category_id != s:transfer_id
-GROUP BY ca.currency
-SQL;
-        $currencies = $this->model->query($sqlCurrencies, [
+        $currencies = $this->model->query("
+            SELECT ca.currency
+            FROM cash_transaction ct
+            JOIN cash_account ca on ca.id = ct.account_id
+            WHERE ct.is_archived = 0
+            AND ca.is_archived = 0
+            AND ca.is_imaginary != -1
+            AND ct.date >= s:date_from
+            AND ct.date <= s:date_to
+            AND ct.category_id != s:transfer_id
+            GROUP BY ca.currency
+        ", [
             'date_from' => $dateFrom->format('Y-m-d'),
             'date_to' => $dateTo->format('Y-m-d'),
             'transfer_id' => cashCategoryFactory::TRANSFER_CATEGORY_ID,
         ])->fetchAll('currency');
-        $currencies = array_column($currencies, 'currency');
 
-        $sqlCategories = <<<SQL
-SELECT cc.*
-FROM cash_transaction ct
-         JOIN cash_account ca on ca.id = ct.account_id
-         JOIN cash_category cc on cc.id = ct.category_id
-WHERE ct.is_archived = 0
-  AND ca.is_archived = 0
-  AND ct.date >= s:date_from
-  AND ct.date <= s:date_to
-  AND ct.category_id != s:transfer_id
-GROUP BY ct.category_id
-SQL;
-        $categories = $this->model->query($sqlCategories, [
+        $currencies = array_column($currencies, 'currency');
+        $categories = $this->model->query("
+            SELECT cc.*
+            FROM cash_transaction ct
+            JOIN cash_account ca on ca.id = ct.account_id
+            JOIN cash_category cc on cc.id = ct.category_id
+            WHERE ct.is_archived = 0
+            AND ca.is_archived = 0
+            AND ca.is_imaginary != -1
+            AND ct.date >= s:date_from
+            AND ct.date <= s:date_to
+            AND ct.category_id != s:transfer_id
+            GROUP BY ct.category_id
+        ", [
             'date_from' => $dateFrom->format('Y-m-d'),
             'date_to' => $dateTo->format('Y-m-d'),
             'transfer_id' => cashCategoryFactory::TRANSFER_CATEGORY_ID,
         ])->fetchAll('id');
 
-        $sql = <<<SQL
-SELECT ca.currency, {$grouping->getSqlGroupBy()} date, ct.category_id, SUM(ABS(ct.amount)) amount
-FROM cash_transaction ct
-         JOIN cash_account ca on ca.id = ct.account_id
-WHERE ct.is_archived = 0
-  AND ca.is_archived = 0
-  AND ct.date >= s:date_from
-  AND ct.date <= s:date_to
-  AND ct.category_id != s:transfer_id
-GROUP BY ca.currency, {$grouping->getSqlGroupBy()}, ct.category_id
-SQL;
-
-        $data = $this->model->query(
-            $sql,
-            [
-                'date_from' => $dateFrom->format('Y-m-d'),
-                'date_to' => $dateTo->format('Y-m-d'),
-                'transfer_id' => cashCategoryFactory::TRANSFER_CATEGORY_ID,
-            ]
-        )->fetchAll('date', 2);
+        $data = $this->model->query("
+            SELECT ca.currency, {$grouping->getSqlGroupBy()} date, ct.category_id, SUM(ABS(ct.amount)) amount
+            FROM cash_transaction ct
+            JOIN cash_account ca on ca.id = ct.account_id
+            WHERE ct.is_archived = 0
+            AND ca.is_archived = 0
+            AND ca.is_imaginary != -1
+            AND ct.date >= s:date_from
+            AND ct.date <= s:date_to
+            AND ct.category_id != s:transfer_id
+            GROUP BY ca.currency, {$grouping->getSqlGroupBy()}, ct.category_id
+        ", [
+            'date_from' => $dateFrom->format('Y-m-d'),
+            'date_to' => $dateTo->format('Y-m-d'),
+            'transfer_id' => cashCategoryFactory::TRANSFER_CATEGORY_ID,
+        ])->fetchAll('date', 2);
 
         $charData = [
             'categories' => $categories,
@@ -95,11 +91,11 @@ SQL;
             $currentDateStr = $grouping->getFormattedDate($currentDate);
 
             foreach ($charData['currencies'] as $currency => $item) {
-                $charData['data'][$currency][$currentDateStr] = array_combine(
+                $charData['data'][(string) $currency][$currentDateStr] = array_combine(
                     $categoryIds,
                     array_fill(0, $categoriesCount, 0.0)
                 );
-                $charData['data'][$currency][$currentDateStr]['date'] = $currentDateStr;
+                $charData['data'][(string) $currency][$currentDateStr]['date'] = $currentDateStr;
             }
 
             if (isset($data[$currentDateStr])) {
@@ -112,7 +108,7 @@ SQL;
         }
 
         foreach ($charData['data'] as $currency => $datum) {
-            $charData['data'][$currency] = array_values($datum);
+            $charData['data'][(string) $currency] = array_values($datum);
         }
 
         return $charData;
