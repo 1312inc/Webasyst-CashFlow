d6ffa4f0e6083e6e4323580efb288d1159fe8ed9
moroz <ivan.moroz@webasyst.ru>
2026-05-20 17:44:24 +0300

    #106.555: clean-up /plans/ SSR prototype

--- lib/config/routing.backend.php
+++ lib/config/routing.backend.php
@@ -4,7 +4,6 @@ return [
     'import/' => 'import/',
     'report/<handler>/<params>' => 'report/',
     'report/' => 'report/',
-    'plan/?' => 'plan/',
     'shop/settings/' => 'shop/settings',
     'loc/' => 'backend/loc',
     'plugins/?' => 'plugins/',
