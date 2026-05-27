84718d9e1557985c3a9aa6b26a61a9631f7c471b
moroz <ivan.moroz@webasyst.ru>
2026-05-27 13:31:22 +0300

    #106.498: cash, plugin, testautomation: add more realistic dummy action & condition, describe general flow/hooks

--- plugins/testautomation/lib/cashTestautomationPlugin.class.php
+++ plugins/testautomation/lib/cashTestautomationPlugin.class.php
@@ -8,17 +8,18 @@ class cashTestautomationPlugin extends waPlugin
             'by_user'  => ['name' => 'Плагин: Имя пользователя', 'operators' => ['==', '<>', '??']],
             'by_inn'   => ['name' => 'Плагин: ИНН', 'operators' => ['^...$', '==', '!=']],
             'by_bank'  => ['name' => 'Плагин: Банк', 'operators' => ['#...#', '==', '!=']],
-            'is_sunny' => ['name' => 'Плагин: Если светит солнце', 'operators' => ['==', '!='], 'select' => ['Ясно', 'Малооблачно', 'Пасмурно']],
+            'is_sunny' => ['name' => 'Плагин: Если светит солнце', 'operators' => ['==', '!='], 'select' => ['clear' => 'Ясно', 'cloudy' => 'Малооблачно', 'gloomy' => 'Пасмурно']],
         ];
     }
 
     private static function getActions()
     {
-        return  [
-            'fetch_commission'   => 'Плагин: Получить и сохранить комиссию маркетплейса',
+        return [
             'repeat_transaction' => 'Плагин: Создавать рекурентную операцию',
             'send_sms'           => 'Плагин: Отправить СМС',
             'create_reminder'    => 'Плагин: Создать напоминание',
+            'create_ozon'        => 'Плагин: Создать новую операцию с комиссией Ozon',
+            'create_wb'          => 'Плагин: Создать новую операцию с комиссией Wildberries',
         ];
     }
 
@@ -50,7 +51,28 @@ class cashTestautomationPlugin extends waPlugin
      */
     public function cashIsConditionTrueTestautomationHandler($params = [])
     {
-        return true;
+        $result = false;
+        $conditions = self::getConditions();
+        $condition_id = ifset($params, 'condition', 'condition_id', null);
+        $operator = ifset($params, 'condition', 'operator', null);
+        $value = ifset($params, 'condition', 'value', null);
+        $transaction = ifset($params, 'transaction', []);
+        if (empty($conditions[$condition_id])) {
+            return false;
+        }
+
+        switch ($condition_id) {
+            case 'by_user':
+            case 'by_inn':
+            case 'by_bank':
+                // проверяем выполнение условия для этих ключей
+                $result = true;
+                break;
+            case 'is_sunny':
+                $result = $this->isSunny($operator, $value, $transaction);
+        }
+
+        return $result;
     }
 
     /**
@@ -72,5 +94,51 @@ class cashTestautomationPlugin extends waPlugin
         cash()->getLogger()->log(['Плагин выполнил: '.$actions[$action], 'PARAMS' => $params], cashAutomation::AUTOMATION_LOG);
 
         return true;
+    }
+
+    /**
+     * Считаем прибыль как "Ясно", а расход как "Пасмурно"
+     *
+     * @param $operator
+     * @param $value
+     * @param $transaction
+     * @return boolean
+     */
+    private function isSunny($operator, $value, $transaction)
+    {
+        $amount = $transaction['amount'];
+        if ($amount == 0) {
+            $_value = 'cloudy';
+        } else {
+            $_value = ($amount > 0 ? 'clear' : 'gloomy');
+        }
+
+        if ($operator === '==' && $value == $_value) {
+            return true;
+        } elseif ($operator === '!=' &&  $value != $_value) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Получение комиссий от Ozon
+     *
+     * @return array
+     */
+    private function getOzonCommission()
+    {
+        return ['20', '14', '4.49', '3.49', '2.39', '1.19'];
+    }
+
+    /**
+     * Получение комиссий от Wildberries
+     *
+     * @return array
+     */
+    private function getWildberriesCommission()
+    {
+        return ['25.5', '27.5', '30.0', '33.4'];
     }
 }
