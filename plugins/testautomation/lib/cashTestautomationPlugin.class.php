ee3551a9ff74ddec90603ac6b99336dfa8228bc5
moroz <ivan.moroz@webasyst.ru>
2026-05-28 17:49:59 +0300

    #106.498: cash, plugin, testautomation: add more realistic dummy action & condition, describe general flow/hooks

--- plugins/testautomation/lib/cashTestautomationPlugin.class.php
+++ plugins/testautomation/lib/cashTestautomationPlugin.class.php
@@ -15,11 +15,11 @@ class cashTestautomationPlugin extends waPlugin
     private static function getActions()
     {
         return [
-            'repeat_transaction' => 'Плагин: Создавать рекурентную операцию',
-            'send_sms'           => 'Плагин: Отправить СМС',
-            'create_reminder'    => 'Плагин: Создать напоминание',
-            'create_ozon'        => 'Плагин: Создать новую операцию с комиссией Ozon',
-            'create_wb'          => 'Плагин: Создать новую операцию с комиссией Wildberries',
+            'repeat_transaction' => ['action' => 'Плагин: Создавать рекурентную операцию'],
+            'send_sms'           => ['action' => 'Плагин: Отправить СМС'],
+            'create_reminder'    => ['action' => 'Плагин: Создать напоминание'],
+            'create_ozon'        => ['action' => 'Плагин: Создать новую операцию с комиссией Ozon'] + self::getElements('create_ozon'),
+            'create_wb'          => ['action' => 'Плагин: Создать новую операцию с комиссией Wildberries'] + self::getElements('create_wb'),
         ];
     }
 
@@ -40,10 +40,13 @@ class cashTestautomationPlugin extends waPlugin
      * Этот метод вызывается для проверки каждого условия, если условие относится к плагину.
      * Выполняется ли, сохраненное в таблице автоматизации, предоставленное плагином условие из getConditions()?
      * $params[
-     *      'action_id'   -> одно из значений transaction_add/transaction_update/transaction_delete
-     *      'action'      -> один из ключей cashAutomation::getActions(), self::getActions() или другого плагина
-     *      'condition'   -> условие для проверки плагином
-     *      'transaction' -> массив с транзакцией
+     *      event_id     -> одно из значений transaction_add/transaction_update/transaction_delete
+     *      action       -> один из ключей cashAutomation::getActions(), self::getActions() или другого плагина
+     *      condition    -> условие для проверки плагином
+     *      transaction  -> массив с транзакцией
+     *      key1 => val1 -> Сохраненные значения дополнительных полей из self::getElements()
+     *      ...
+     *      keyN => valN
      * ]
      * В ответе, метод возвращает true, если условие по мнению плагина истинное
      *
@@ -81,19 +84,45 @@ class cashTestautomationPlugin extends waPlugin
      *
      * @param $params
      * @return bool
+     * @throws waException
      */
     public function cashEventTestautomationHandler($params = [])
     {
         $actions = self::getActions();
         $action = ifset($params, 'action', '');
+        $transaction = ifset($params, 'transaction', []);
         if (empty($actions[$action])) {
             cash()->getLogger()->log(['В плагине нет такого действия для выполнения', 'PARAMS' => $params], cashAutomation::AUTOMATION_LOG);
             return false;
         }
+        if ($action === 'create_wb' || $action === 'create_ozon') {
+            $c = ($action === 'create_wb' ? $this->getWildberriesCommission() : $this->getOzonCommission());
+            $commission = $c[array_rand($c)];
 
-        cash()->getLogger()->log(['Плагин выполнил: '.$actions[$action], 'PARAMS' => $params], cashAutomation::AUTOMATION_LOG);
+            $desc = sprintf(
+                'Коммисия %s %s%% в размере %s от суммы %s',
+                $action === 'create_wb' ? 'Wildberries' : 'Ozon',
+                $commission,
+                ($transaction['amount']/100)*$commission,
+                $transaction['amount']
+            );
 
-        return true;
+            $new_transaction = (cash()->getEntityFactory(cashTransaction::class))->createNew();
+            $new_transaction->setAmount(($transaction['amount']/100)*$commission);
+            $new_transaction->setDescription($desc);
+            $new_transaction->setDate(date('Y-m-d'));
+            $new_transaction->setAccountId(ifset($transaction, 'account_id', null));
+            $new_transaction->setCategoryId(ifset($params, 'expense_category', null));
+
+            $saver = new cashTransactionSaver();
+            $saver->addToPersist($new_transaction);
+            $saver->persistTransactions();
+
+            cash()->getLogger()->log(['Плагин выполнил: '.$actions[$action]['action'], 'PARAMS' => $params], cashAutomation::AUTOMATION_LOG);
+            return true;
+        }
+
+        return false;
     }
 
     /**
@@ -140,5 +169,32 @@ class cashTestautomationPlugin extends waPlugin
     private function getWildberriesCommission()
     {
         return ['25.5', '27.5', '30.0', '33.4'];
+    }
+
+    /**
+     * Добавление расширенных полей в "Действия"
+     *
+     * @param $type
+     * @return array[]
+     * @throws waException
+     */
+    private static function getElements($type)
+    {
+        $elements = [];
+        switch ($type) {
+            case 'create_ozon':
+            case 'create_wb':
+                $categories = cash()->getModel(cashCategory::class)->getByTypeForContact(cashCategory::TYPE_EXPENSE);
+                $elements = [
+                    'expense_category' => [
+                        'type' => 'select',
+                        'label' => 'Категория для операции',
+                        'options' => ['' => 'Выбрать категорию'] + array_combine(array_column($categories, 'id'), array_column($categories, 'name'))
+                    ],
+                ];
+                break;
+        }
+
+        return ['elements' => $elements];
     }
 }
