f8a3619914b85c467c2427f78db79ae500004c8b
moroz <ivan.moroz@webasyst.ru>
2026-07-08 12:54:09 +0300

    Revert "#106.596: automation > rule processing order > swap 'sort'"
    This reverts commit 9505f90fbb477cd27647daf3e21d331804c909df.

--- lib/class/cashAutomation.class.php
+++ lib/class/cashAutomation.class.php
@@ -295,7 +295,6 @@ class cashAutomation
                                     $plugin_view = ifset($plugin_view, 'actions', $params['action'], 'action', null);
                                 }
                                 self::getLog()->add($rule, $transaction, sprintf_wp('Действие %s плагином выполнено', (empty($plugin_view) ? '' : $plugin_view)));
-                                break;
                             }
                         } catch (Exception $ex) {
                             self::getLog()->add($rule, $transaction, $ex->getMessage(), 'error');
@@ -326,7 +325,6 @@ class cashAutomation
                     }
                     if ($done) {
                         self::getLog()->add($rule, $transaction, sprintf_wp('Действие "%s" выполнено', ifset($known_actions, $rule_action, 'action', _w('Неизвестное действие'))));
-                        break;
                     }
                 }
             }
