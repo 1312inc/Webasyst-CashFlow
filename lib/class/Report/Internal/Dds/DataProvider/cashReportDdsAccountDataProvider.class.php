988da5cf206ae40a7a4dae8f899e581d68f71c5e
moroz <ivan.moroz@webasyst.ru>
2026-07-24 15:03:08 +0300

    #106.543: /report/dds/ > year total > use H logic, not V

--- lib/class/Report/Internal/Dds/DataProvider/cashReportDdsAccountDataProvider.class.php
+++ lib/class/Report/Internal/Dds/DataProvider/cashReportDdsAccountDataProvider.class.php
@@ -94,11 +94,9 @@ final class cashReportDdsAccountDataProvider implements cashReportDdsDataProvide
                 || 1 === (int) $datum['is_imaginary'] && $datum['month'] > $current_month
             ) {
                 $rawData[$catType][$categoryTypeKey][$month][$currency]['per_month'] += (float) $perMonth;
+                $rawData[$catType][$categoryTypeKey]['total'][$currency]['per_month'] += (float) $perMonth;
             } else {
                 $rawData[$catType][$categoryTypeKey][$month][$currency]['imaginary'] = (int) $datum['is_imaginary'];
-            }
-            if (0 === (int) $datum['is_imaginary']) {
-                $rawData[$catType][$categoryTypeKey]['total'][$currency]['per_month'] += (float) $perMonth;
             }
         }
 
