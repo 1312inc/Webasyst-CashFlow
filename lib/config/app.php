620e5e1daaa2bd8631a5e0c18681ee0476b10bd3
Vladimir V. Tuporshin <vofka@vofka.ru>
2026-07-29 15:22:08 +0300

    4.1.0 app version up!

--- lib/config/app.php
+++ lib/config/app.php
@@ -2,7 +2,7 @@
 return array (
   'name' => 'Cash Flow',
   'icon' => 'img/cash.svg',
-  'version' => '4.0.0',
+  'version' => '4.1.0',
   'vendor'  => '1021997',
   'rights'  => true,
   'sash_color' => '#16d15f',
